37d2be74d098f982aeb6d919585028c5c6cb79a8
josh <josh@launchbadge.com>
2020-01-06 14:14:25 -0800

    update tests

--- account_delete_transaction_test.go
+++ account_delete_transaction_test.go
@@ -13,18 +13,18 @@ func TestSerializeAccountDeleteTransaction(t *testing.T) {
 	assert.NoError(t, err)
 
 	tx := NewAccountDeleteTransaction().
-		SetAccountId(AccountID{ Account: 3 }).
-		// todo: transfer account id
+		SetDeleteAccountId(AccountID{ Account: 3 }).
+		SetTransferAccountID(AccountID{ Account: 2 }).
 		SetMaxTransactionFee(1e6).
 		SetTransactionID(testTransactionId).
 		Build(mockClient)
 
 	tx.Sign(privateKey)
 
-	txString := `bodyBytes: "\n\016\n\010\010\334\311\007\020\333\237\t\022\002\030\003\022\002\030\003\030\300\204=\"\002\010xb\004\022\002\030\003"
+	txString := `bodyBytes: "\n\016\n\010\010\334\311\007\020\333\237\t\022\002\030\003\022\002\030\003\030\300\204=\"\002\010xb\010\n\002\030\002\022\002\030\003"
 sigMap: <
   sigPair: <
-    ed25519: "IZ\225\325\352\274\200K~\207\321>\204\034\003\231O\254\2435\324\257a\033\272\247\031\231\240R\303\375\356m\373\320\235\312\353\363\027\2411'\362*\236#\215\362\372;f\342\332\257=W\333n\213\017*\002"
+    ed25519: "&\321\261A\177f\316\346\326\346\t\004\202\272\365Q_/\027\014-:\3429eM\265\263\275N\227\350?G\270f\347\205mk0\211zH\3244w\221\213\005\315\1776\236~Z\341\2138\277TLF\007"
   >
 >
 transactionID: <
@@ -44,11 +44,15 @@ transactionValidDuration: <
   seconds: 120
 >
 cryptoDelete: <
+  transferAccountID: <
+    accountNum: 2
+  >
   deleteAccountID: <
     accountNum: 3
   >
 >
 `
+
 
 	assert.Equal(t, txString, tx.String())
 }
