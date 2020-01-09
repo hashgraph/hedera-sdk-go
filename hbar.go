87dc24b4ca9335c24cd54078c37a8c80e20ddebf
Daniel Akhterov <daniel@launchbadge.com>
2020-01-08 23:31:02 -0800

    Hbar Parity
        - Implement `HbarFromDecimal()`
        - Implement `Hbar.String()`
        - Implement `Hbar.As()`
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- hbar.go
+++ hbar.go
@@ -1,24 +1,43 @@
 package hedera
 
+import "fmt"
+
 type Hbar struct {
 	tinybar int64
+	unit    HbarUnit
 }
 
 const max = int64(^uint(0) >> 1)
 const min = -max - 1
 
-var MaxHbar = Hbar{max}
-var MinHbar = Hbar{min}
-var ZeroHbar = Hbar{}
+var MaxHbar = Hbar{max, HBar}
+var MinHbar = Hbar{min, HBar}
+var ZeroHbar = Hbar{0, HBar}
+
+func HbarFromDecimal(bars float64, unit HbarUnit) Hbar {
+	return Hbar{int64(bars * float64(unit.toTinybarCount())), unit}
+}
 
 func HbarFromTinybar(tinybar int64) Hbar {
-	return Hbar{tinybar: tinybar}
+	return Hbar{tinybar: tinybar, unit: Tinybar}
 }
 
 func HbarOf(hbar float64) Hbar {
-	return Hbar{tinybar: int64(hbar * 100_000_000)}
+	return Hbar{tinybar: int64(hbar * 100_000_000), unit: HBar}
 }
 
 func (hbar Hbar) AsTinybar() uint64 {
 	return uint64(hbar.tinybar)
+}
+
+func (hbar Hbar) As(unit HbarUnit) int64 {
+	return hbar.tinybar * hbar.unit.toTinybarCount()
+}
+
+func (hbar Hbar) String() string {
+	if hbar.unit == Tinybar {
+		return fmt.Sprintf("%v %v", hbar.tinybar, hbar.unit.String())
+	} else {
+		return fmt.Sprintf("%v %v (%v tinybar)", hbar.tinybar, hbar.unit.String(), hbar.As(Tinybar))
+	}
 }
--- hbar_unit.go
+++ hbar_unit.go
@@ -36,3 +36,24 @@ func (unit HbarUnit) Symbol() string {
 func (unit HbarUnit) String() string {
 	return string(unit)
 }
+
+func (unit HbarUnit) toTinybarCount() int64 {
+	switch unit {
+	case Tinybar:
+		return 1
+	case Microbar:
+		return 100
+	case Millibar:
+		return 100_000
+	case HBar:
+		return 100_000_000
+	case Kilobar:
+		return 100_000_000_000
+	case Megabar:
+		return 100_000_000_000_000
+	case Gigabar:
+		return 100_000_000_000_000_000
+	}
+
+	panic("HbarUnit.Symbol() switch statement is non-exhaustive")
+}
