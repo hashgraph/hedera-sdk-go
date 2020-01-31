e536e1ad4423c8e85d5b237dc584fa8aa972e06f
Ryan Leckey <ryan@launchbadge.com>
2020-01-31 15:47:30 -0800

    Add GenerateWithMnemonic example

---
+++ examples/generate_key_with_mnemonic/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hashgraph/hedera-sdk-go"
+)
+
+func main() {
+	mnemonic, err := hedera.GenerateMnemonic()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("mnemonic = %v\n", mnemonic)
+
+	privateKey, err := mnemonic.ToPrivateKey( /* passphrase */ "")
+	if err != nil {
+		panic(err)
+	}
+
+	publicKey := privateKey.PublicKey()
+
+	fmt.Printf("private = %v\n", privateKey)
+	fmt.Printf("public = %v\n", publicKey)
+}
