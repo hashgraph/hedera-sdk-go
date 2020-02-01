85c0f4a980071e1d5d8a92894f3826a7b336e85a
josh <josh@launchbadge.com>
2020-01-31 20:13:17 -0800

    add get file contents example

---
+++ examples/get_file_contents/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hashgraph/hedera-sdk-go"
+	"os"
+)
+
+func main() {
+	client := hedera.ClientForTestnet()
+
+	operatorPrivateKey, err := hedera.Ed25519PrivateKeyFromString(os.Getenv("OPERATOR_KEY"))
+	if err != nil {
+		panic(err)
+	}
+
+	operatorAccountID, err := hedera.AccountIDFromString(os.Getenv("OPERATOR_ID"))
+	if err != nil {
+		panic(err)
+	}
+
+	// Constructors exist for convenient files
+	fileID := hedera.FileIDForAddressBook()
+	// fileID := hedera.FileIDForFeeSchedule()
+	// fileID := hedera.FileIDForExchangeRate()
+
+	client.SetOperator(operatorAccountID, operatorPrivateKey)
+
+	contents, err := hedera.NewFileContentsQuery().
+		SetFileID(fileID).
+		Execute(client)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("contents for file %v :\n", fileID)
+	fmt.Print(contents)
+	fmt.Println()
+}
