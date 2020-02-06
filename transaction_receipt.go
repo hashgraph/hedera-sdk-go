2a75fb8fcb3bbd62bb9a98e3b4ddb354d90216bc
josh <josh@launchbadge.com>
2020-02-05 18:22:34 -0800

    improve TransactionReceipt - add tryGet functions for possible nil values - make ConsensusTopicSequenceNUmber and ConsensusTopicRunningHash private - add missing getters - add documentation for public functions and values

--- transaction_receipt.go
+++ transaction_receipt.go
@@ -1,33 +1,113 @@
 package hedera
 
 import (
+	"fmt"
 	"github.com/hashgraph/hedera-sdk-go/proto"
 )
 
+// TransactionReceipt is the consensus result for a transaction which is returned from a TransactionReceiptQuery.
 type TransactionReceipt struct {
+	// Status is the consensus status of the receipt's transaction. It might be unknown or have Failed.
 	Status                       Status
 	accountID                    *AccountID
 	contractID                   *ContractID
 	fileID                       *FileID
 	consensusTopicID             *ConsensusTopicID
-	ConsensusTopicSequenceNumber uint64
-	ConsensusTopicRunningHash    []byte
+	consensusTopicSequenceNumber uint64
+	consensusTopicRunningHash    []byte
 }
 
+// GetFileID returns the FileID associated with the receipt's transaction or else panics no FileID exists
 func (receipt TransactionReceipt) GetFileID() FileID {
 	return *receipt.fileID
 }
 
+// TryGetFileID returns the FileID associated with the receipt's transaction or else returns an err if no FileID exists.
+func (receipt TransactionReceipt) TryGetFileID() (FileID, error) {
+	if receipt.fileID == nil {
+		return FileID{}, fmt.Errorf("no file id exists on this receipt")
+	}
+
+	return receipt.GetFileID(), nil
+}
+
+// GetAccountID returns the AccountID associated with the receipt's transaction or else panics if no AccountID exists
 func (receipt TransactionReceipt) GetAccountID() AccountID {
 	return *receipt.accountID
 }
 
+// TryGetAccountID returns the AccountID associated with the receipt's transaction or else returns an error
+// if no AccountID exists
+func (receipt TransactionReceipt) TryGetAccountID() (AccountID, error) {
+	if receipt.accountID == nil {
+		return AccountID{}, fmt.Errorf("no account id exists on this receipt")
+	}
+
+	return receipt.GetAccountID(), nil
+}
+
+// GetContractID returns the ContractID associated with the receipt's transaction or else panics if no ContractID exists
 func (receipt TransactionReceipt) GetContractID() ContractID {
 	return *receipt.contractID
 }
 
-func (receipt TransactionReceipt) ConsensusTopicID() ConsensusTopicID {
+// TryGetContractID returns the ContractID associated with the receipt's transaction or else returns an error
+// if no ContractID exists
+func (receipt TransactionReceipt) TryGetContractID() (ContractID, error) {
+	if receipt.contractID == nil {
+		return ContractID{}, fmt.Errorf("no contract id exists on this receipt")
+	}
+
+	return receipt.GetContractID(), nil
+}
+
+// GetConsensusTopicID returns the ConsensusTopicID associated with the receipt's transaction or else panics
+// if no ConsensusTopicID exists
+func (receipt TransactionReceipt) GetConsensusTopicID() ConsensusTopicID {
 	return *receipt.consensusTopicID
+}
+
+// TryGetConsensusTopicID returns the ConsensusTopicID associated with the receipt's transaction or else
+// returns an error if no ConsensusTopicID exists
+func (receipt TransactionReceipt) TryGetConsensusTopicID() (ConsensusTopicID, error) {
+	if receipt.consensusTopicID == nil {
+		return ConsensusTopicID{}, fmt.Errorf("no consensus id exists on this receipt")
+	}
+	return receipt.GetConsensusTopicID(), nil
+}
+
+// GetConsensusTopicSequenceNumber returns the topic sequence number associated with the
+// Consensus Topic. However, if a ConsensusTopicID does not exist on the receipt it will return
+// potentially invalid values.
+func (receipt TransactionReceipt) GetConsensusTopicSequenceNumber() uint64 {
+	return receipt.consensusTopicSequenceNumber
+}
+
+// TryGetConsensusTopicSequenceNumber checks if the receipt contains a ConsensusTopicID. If
+// the ConsensusTopicID exists it will return the ConsensusTopicSequenceNumber. Otherwise an
+// error will be returned.
+func (receipt TransactionReceipt) TryGetConsensusTopicSequenceNumber() (uint64, error) {
+	if _, err := receipt.TryGetConsensusTopicID(); err != nil {
+		return 0, err
+	}
+	return receipt.GetConsensusTopicSequenceNumber(), nil
+}
+
+// GetConsensusTopicRunningHash returns the running hash associated with the Consensus Topic.
+// However, if a ConsensusTopicID does not exist on the receipt it will return potentially
+// invalid values (likely an empty slice).
+func (receipt TransactionReceipt) GetConsensusTopicRunningHash() []byte {
+	return receipt.consensusTopicRunningHash
+}
+
+// TryGetConsensusTopicRunningHash checks if the receipt contains a ConsensusTopicID. If the
+// ConsensusTopicID exists it will return the running hash associated with the consensus Topic.
+// Otherwise, an error will be returned.
+func (receipt TransactionReceipt) TryGetConsensusTopicRunningHash() ([]byte, error) {
+	if _, err := receipt.TryGetConsensusTopicID(); err != nil {
+		return []byte{}, err
+	}
+	return receipt.consensusTopicRunningHash, nil
 }
 
 func transactionReceiptFromResponse(response *proto.Response) TransactionReceipt {
@@ -65,7 +145,7 @@ func transactionReceiptFromProto(pb *proto.TransactionReceipt) TransactionReceip
 		contractID:                   contractID,
 		fileID:                       fileID,
 		consensusTopicID:             consensusTopicID,
-		ConsensusTopicSequenceNumber: pb.TopicSequenceNumber,
-		ConsensusTopicRunningHash:    pb.TopicRunningHash,
+		consensusTopicSequenceNumber: pb.TopicSequenceNumber,
+		consensusTopicRunningHash:    pb.TopicRunningHash,
 	}
 }
