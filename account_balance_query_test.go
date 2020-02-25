1eba364b4a510139497b1e239988254d60690a7d
josh <josh@launchbadge.com>
2020-02-24 18:22:03 -0800

    account balance query e2e

--- account_balance_query_test.go
+++ account_balance_query_test.go
@@ -3,6 +3,7 @@ package hedera
 import (
 	"github.com/bradleyjkemp/cupaloy"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
@@ -26,4 +27,56 @@ func TestNewAccountBalanceQuery_ForContract(t *testing.T) {
 		SetQueryPaymentTransaction(mockTransaction)
 
 	cupaloy.SnapshotT(t, query.pb.String())
+}
+
+func TestAccountBalanceQuery_Execute(t *testing.T) {
+	operatorAccountID, err := AccountIDFromString(os.Getenv("OPERATOR_ID"))
+	assert.NoError(t, err)
+
+	operatorPrivateKey, err := Ed25519PrivateKeyFromString(os.Getenv("OPERATOR_KEY"))
+	assert.NoError(t, err)
+
+	newKey, err := GenerateEd25519PrivateKey()
+	assert.NoError(t, err)
+
+	client := ClientForTestnet().SetOperator(operatorAccountID, operatorPrivateKey)
+
+	newBalance := NewHbar(1)
+
+	assert.Equal(t, HbarUnits.Hbar.numberOfTinybar(), newBalance.tinybar)
+
+	txID, err := NewAccountCreateTransaction().
+		SetKey(newKey.PublicKey()).
+		SetMaxTransactionFee(NewHbar(2)).
+		SetInitialBalance(newBalance).
+		Execute(client)
+	assert.NoError(t, err)
+
+	receipt, err := txID.GetReceipt(client)
+	assert.NoError(t, err)
+
+	accountID := receipt.GetAccountID()
+	assert.NoError(t, err)
+
+	balance, err := NewAccountBalanceQuery().
+		SetAccountID(accountID).
+		Execute(client)
+	assert.NoError(t, err)
+	assert.Equal(t, newBalance, balance)
+
+	tx, err := NewAccountDeleteTransaction().
+		SetDeleteAccountID(accountID).
+		SetTransferAccountID(operatorAccountID).
+		SetMaxTransactionFee(NewHbar(1)).
+		SetTransactionID(NewTransactionID(accountID)).
+		Build(client)
+	assert.NoError(t, err)
+
+	txID, err = tx.
+		Sign(newKey).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_, err = txID.GetReceipt(client)
+	assert.NoError(t, err)
 }
