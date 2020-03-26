3eabaf495c940762baf46ddee5fe73db963d9830
Josh <josh@launchbadge.com>
2020-03-25 20:38:29 -0700

    fix and add missing file * tests

--- file_append_transaction_test.go
+++ file_append_transaction_test.go
@@ -2,6 +2,7 @@ package hedera
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
 	"strings"
 	"testing"
 )
@@ -25,4 +26,56 @@ func TestSerializeFileAppendTransaction(t *testing.T) {
 	tx.Sign(privateKey)
 
 	assert.Equal(t, `bodyBytes:"\n\016\n\010\010\334\311\007\020\333\237\t\022\002\030\003\022\002\030\003\030\300\204=\"\002\010x\202\001\036\022\002\030\005\"\030Thisissomerandomdata"sigMap:<sigPair:<pubKeyPrefix:"\344\361\300\353L}\315\303\347\353\021p\263\010\212=\022\242\227\364\243\353\342\362\205\003\375g5F\355\216"ed25519:"\211Q\013\365\342\\\257B\255\370\347t\234`+"`"+`)\271\017\\\273\266\367\347\214\256]D\2004\220kC$:\252\245\227\257\351\365\344\236\244\032\336@\263a\353\001\276\257\300)x\254\021\032\217\223DF\316\016\004">>transactionID:<transactionValidStart:<seconds:124124nanos:151515>accountID:<accountNum:3>>nodeAccountID:<accountNum:3>transactionFee:1000000transactionValidDuration:<seconds:120>fileAppend:<fileID:<fileNum:5>contents:"Thisissomerandomdata">`, strings.ReplaceAll(strings.ReplaceAll(tx.String(), " ", ""), "\n", ""))
+}
+
+func TestFileAppendTransaction_Execute(t *testing.T) {
+	operatorAccountID, err := AccountIDFromString(os.Getenv("OPERATOR_ID"))
+	assert.NoError(t, err)
+
+	operatorPrivateKey, err := Ed25519PrivateKeyFromString(os.Getenv("OPERATOR_KEY"))
+	assert.NoError(t, err)
+
+	client := ClientForTestnet().
+		SetOperator(operatorAccountID, operatorPrivateKey).
+		SetMaxTransactionFee(NewHbar(2))
+
+	txID, err := NewFileCreateTransaction().
+		AddKey(operatorPrivateKey.PublicKey()).
+		SetContents([]byte("Hello")).
+		SetTransactionMemo("go sdk e2e tests").
+		Execute(client)
+
+	assert.NoError(t, err)
+
+	receipt, err := txID.GetReceipt(client)
+	assert.NoError(t, err)
+
+	fileID := receipt.fileID
+	assert.NotNil(t, fileID)
+
+
+	txID, err = NewFileAppendTransaction().
+		SetFileID(*fileID).
+		SetContents([]byte(" world!")).
+		Execute(client)
+
+	assert.NoError(t, err)
+
+	_, err = txID.GetReceipt(client)
+	assert.NoError(t, err)
+
+	contents, err := NewFileContentsQuery().
+		SetFileID(*fileID).
+		Execute(client)
+	assert.NoError(t, err)
+
+	assert.Equal(t, []byte("Hello world!"), contents)
+
+	txID, err = NewFileDeleteTransaction().
+		SetFileID(*fileID).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_, err = txID.GetReceipt(client)
+	assert.NoError(t, err)
 }
--- file_info_query_test.go
+++ file_info_query_test.go
@@ -2,6 +2,7 @@ package hedera
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
 	"strings"
 	"testing"
 )
@@ -15,4 +16,45 @@ func TestNewFileInfoQuery(t *testing.T) {
 		SetQueryPaymentTransaction(mockTransaction)
 
 	assert.Equal(t, `fileGetInfo:{header:{payment:{bodyBytes:"\n\x0e\n\x08\x08\xdc\xc9\x07\x10۟\t\x12\x02\x18\x03\x12\x02\x18\x03\x18\x80\xc2\xd7/\"\x02\x08xr\x14\n\x12\n\x07\n\x02\x18\x02\x10\xc7\x01\n\x07\n\x02\x18\x03\x10\xc8\x01"sigMap:{sigPair:{pubKeyPrefix:"\xe4\xf1\xc0\xebL}\xcd\xc3\xe7\xeb\x11p\xb3\x08\x8a=\x12\xa2\x97\xf4\xa3\xeb\xe2\xf2\x85\x03\xfdg5F\xed\x8e"ed25519:"\x12&5\x96\xfb\xb4\x1c]P\xbb%\xecP\x9bk͙\x0b߼\xac)\xa6+\xd2<\x97+\xbb\x8c\x8af\xcb\xdai\x17T4{\xf7\xf3UYn\n\x8f\xabep\x04\xf6\x83\x0f\xbaFUP\xa3\xd1/\x1d\x9d\x1a\x0b"}}}}fileID:{fileNum:3}}`, strings.ReplaceAll(query.QueryBuilder.pb.String(), " ", ""))
+}
+
+func TestFileInfoQueryTransaction_Execute(t *testing.T) {
+	operatorAccountID, err := AccountIDFromString(os.Getenv("OPERATOR_ID"))
+	assert.NoError(t, err)
+
+	operatorPrivateKey, err := Ed25519PrivateKeyFromString(os.Getenv("OPERATOR_KEY"))
+	assert.NoError(t, err)
+
+	client := ClientForTestnet().
+		SetOperator(operatorAccountID, operatorPrivateKey).
+		SetMaxTransactionFee(NewHbar(2))
+
+	txID, err := NewFileCreateTransaction().
+		AddKey(operatorPrivateKey.PublicKey()).
+		SetContents([]byte("Hello, World")).
+		SetTransactionMemo("go sdk e2e tests").
+		Execute(client)
+
+	assert.NoError(t, err)
+
+	receipt, err := txID.GetReceipt(client)
+	assert.NoError(t, err)
+
+	fileID := receipt.fileID
+	assert.NotNil(t, fileID)
+
+	info, err := NewFileInfoQuery().
+		SetFileID(*fileID).
+		Execute(client)
+	assert.NoError(t, err)
+
+	assert.Equal(t, *fileID, info.FileID)
+
+	txID, err = NewFileDeleteTransaction().
+		SetFileID(*fileID).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_, err = txID.GetReceipt(client)
+	assert.NoError(t, err)
 }
