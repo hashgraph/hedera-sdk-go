cc9bc84c0ffbd8323ec351b589bc09ae37ba87b9
Josh <josh@launchbadge.com>
2020-03-31 14:44:54 -0700

    topic update test

--- consensus_topic_update_transaction_test.go
+++ consensus_topic_update_transaction_test.go
@@ -1,6 +1,7 @@
 package hedera
 
 import (
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -32,4 +33,70 @@ func TestSerializeConsensusTopicUpdateTransaction(t *testing.T) {
 	tx.Sign(key)
 
 	assert.Equal(t, `bodyBytes:"\n\014\n\006\010\316\247\212\345\005\022\002\030\002\022\002\030\003\030\300\204=\"\004\010\200\243\005\312\001>\n\002\030c\022\024\n\022updatedtopicmemo2\"\022\344\361\300\353L}\315\303\347\353\021p\263\010\212=\022\242\227\364\243\353\342\362\205\003\375g5F\355\216"sigMap:<sigPair:<pubKeyPrefix:"\344\361\300\353L}\315\303\347\353\021p\263\010\212=\022\242\227\364\243\353\342\362\205\003\375g5F\355\216"ed25519:"\331\274\230\260l\"l\322\265\245\225\265\r\221\250N5\316gSL\004\003\237X\325\220\226\314O$\344poH\322\330\226\013\021\253.\222i\355\202\303\360\016\3036\213'\003T\033\2121\213\376\2650\010">>transactionID:<transactionValidStart:<seconds:1554158542>accountID:<accountNum:2>>nodeAccountID:<accountNum:3>transactionFee:1000000transactionValidDuration:<seconds:86400>consensusUpdateTopic:<topicID:<topicNum:99>memo:<value:"updatedtopicmemo">adminKey:<ed25519:"\344\361\300\353L}\315\303\347\353\021p\263\010\212=\022\242\227\364\243\353\342\362\205\003\375g5F\355\216">>`, strings.ReplaceAll(strings.ReplaceAll(tx.String(), " ", ""), "\n", ""))
+}
+
+func TestConsensusTopicUpdateTransaction_Execute(t *testing.T) {
+	operatorAccountID, err := AccountIDFromString(os.Getenv("OPERATOR_ID"))
+	assert.NoError(t, err)
+
+	operatorPrivateKey, err := Ed25519PrivateKeyFromString(os.Getenv("OPERATOR_KEY"))
+	assert.NoError(t, err)
+
+	client := ClientForTestnet().
+		SetOperator(operatorAccountID, operatorPrivateKey)
+
+	oldTopicMemo := "go-sdk::TestConsensusTopicUpdateTransaction_Execute::initial"
+
+	txID, err := NewConsensusTopicCreateTransaction().
+		SetAdminKey(operatorPrivateKey.PublicKey()).
+		SetTopicMemo(oldTopicMemo).
+		SetMaxTransactionFee(NewHbar(1)).
+		Execute(client)
+	assert.NoError(t, err)
+
+	receipt, err := txID.GetReceipt(client)
+	assert.NoError(t, err)
+
+	topicID := receipt.GetConsensusTopicID()
+	assert.NotNil(t, topicID)
+
+	info, err := NewConsensusTopicInfoQuery().
+		SetTopicID(topicID).
+		SetMaxQueryPayment(NewHbar(1)).
+		Execute(client)
+	assert.NoError(t, err)
+	assert.NotNil(t, info)
+
+	assert.Equal(t, oldTopicMemo, info.Memo)
+	assert.Equal(t, uint64(0), info.SequenceNumber)
+	assert.Equal(t, operatorPrivateKey.PublicKey().String(), info.AdminKey.String())
+
+	newTopicMemo := "go-sdk::TestConsensusTopicUpdateTransaction_Execute::updated"
+
+	txID, err = NewConsensusTopicUpdateTransaction().
+		SetTopicID(topicID).
+		SetTopicMemo(newTopicMemo).
+		SetMaxTransactionFee(NewHbar(1)).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_, err = txID.GetReceipt(client)
+	assert.NoError(t, err)
+
+	info, err = NewConsensusTopicInfoQuery().
+		SetTopicID(topicID).
+		SetMaxQueryPayment(NewHbar(1)).
+		Execute(client)
+	assert.NoError(t, err)
+	assert.NotNil(t, info)
+
+	assert.Equal(t, newTopicMemo, info.Memo)
+	assert.Equal(t, uint64(0), info.SequenceNumber)
+	assert.Equal(t, operatorPrivateKey.PublicKey().String(), info.AdminKey.String())
+
+	_, err = NewConsensusTopicDeleteTransaction().
+		SetTopicID(topicID).
+		SetMaxTransactionFee(NewHbar(1)).
+		Execute(client)
+	assert.NoError(t, err)
 }
