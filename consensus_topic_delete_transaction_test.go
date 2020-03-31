66bc411bbcf541a48f175f4e7877e83eee111fb2
Josh <josh@launchbadge.com>
2020-03-31 13:53:12 -0700

    topic delete test

--- consensus_topic_delete_transaction_test.go
+++ consensus_topic_delete_transaction_test.go
@@ -1,6 +1,7 @@
 package hedera
 
 import (
+	"os"
 	"strings"
 	"testing"
 
@@ -33,4 +34,54 @@ func TestSerializeConsensusTopicDeleteTransaction(t *testing.T) {
 	tx.Sign(key)
 
 	assert.Equal(t, `bodyBytes:"\n\014\n\006\010\316\247\212\345\005\022\002\030\002\022\002\030\003\030\300\204=\"\004\010\200\243\005\322\001\004\n\002\030c"sigMap:<sigPair:<pubKeyPrefix:"\344\361\300\353L}\315\303\347\353\021p\263\010\212=\022\242\227\364\243\353\342\362\205\003\375g5F\355\216"ed25519:"\340\240!e\360j(\230Y\352q\213\334,\335`+"`"+`\236v\247\326T\026c-\373&\024\004~\036P\304\024u\211\220\370\306R\267\024<\372\3220\272\205?\335J^:=\232\021NU2\207\315r\024\317\013">>transactionID:<transactionValidStart:<seconds:1554158542>accountID:<accountNum:2>>nodeAccountID:<accountNum:3>transactionFee:1000000transactionValidDuration:<seconds:86400>consensusDeleteTopic:<topicID:<topicNum:99>>`, strings.ReplaceAll(strings.ReplaceAll(tx.String(), " ", ""), "\n", ""))
+}
+
+func TestConsensusTopicDeleteTransaction_Execute(t *testing.T) {
+	operatorAccountID, err := AccountIDFromString(os.Getenv("OPERATOR_ID"))
+	assert.NoError(t, err)
+
+	operatorPrivateKey, err := Ed25519PrivateKeyFromString(os.Getenv("OPERATOR_KEY"))
+	assert.NoError(t, err)
+
+	client := ClientForTestnet().
+		SetOperator(operatorAccountID, operatorPrivateKey)
+
+	topicMemo := "go-sdk::TestConsensusTopicDeleteTransaction_Execute"
+
+	txID, err := NewConsensusTopicCreateTransaction().
+		SetAdminKey(operatorPrivateKey.PublicKey()).
+		SetTopicMemo(topicMemo).
+		SetMaxTransactionFee(NewHbar(1)).
+		Execute(client)
+	assert.NoError(t, err)
+
+	receipt, err := txID.GetReceipt(client)
+	assert.NoError(t, err)
+
+	topicID := receipt.GetConsensusTopicID()
+	assert.NotNil(t, topicID)
+
+	_, err = NewConsensusTopicInfoQuery().
+		SetTopicID(topicID).
+		SetMaxQueryPayment(NewHbar(1)).
+		Execute(client)
+	assert.NoError(t, err)
+
+	txID, err = NewConsensusTopicDeleteTransaction().
+		SetTopicID(topicID).
+		SetMaxTransactionFee(NewHbar(1)).
+		Execute(client)
+	assert.NoError(t, err)
+
+	receipt, err = txID.GetReceipt(client)
+	assert.NoError(t, err)
+
+	_, err = NewConsensusTopicInfoQuery().
+		SetTopicID(topicID).
+		SetMaxQueryPayment(NewHbar(1)).
+		Execute(client)
+	assert.Error(t, err)
+
+	status := err.(ErrHederaPreCheckStatus).Status
+	assert.Equal(t, StatusInvalidTopicID, status)
 }
