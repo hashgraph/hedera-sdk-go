6383c99957974aa89d6c15f9b7bfdf533a387de4
Andrei Kuzmiankov <ender2016@gmail.com>
2020-10-16 04:14:26 -0700

    feat: Added network_version_info_query

---
+++ network_version_info_query.go
@@ -0,0 +1,97 @@
+package hedera
+
+import "github.com/hashgraph/hedera-sdk-go/proto"
+
+type NetworkVersionInfoQuery struct {
+	Query
+	pb *proto.NetworkGetVersionInfoQuery
+}
+
+func NewNetworkVersionQuery() *NetworkVersionInfoQuery {
+	header := proto.QueryHeader{}
+	query := newQuery(true, &header)
+	pb := proto.NetworkGetVersionInfoQuery{Header: &header}
+	query.pb.Query = &proto.Query_NetworkGetVersionInfo{
+		NetworkGetVersionInfo: &pb,
+	}
+
+	return &NetworkVersionInfoQuery{
+		Query: query,
+		pb:    &pb,
+	}
+}
+
+func networkVersionInfoQuery_mapResponseStatus(_ request, response response) Status {
+	return Status(response.query.GetNetworkGetVersionInfo().Header.NodeTransactionPrecheckCode)
+}
+
+func networkVersionInfoQuery_getMethod(_ request, channel *channel) method {
+	return method{
+		query: channel.getNetwork().GetVersionInfo,
+	}
+}
+
+func (query *NetworkVersionInfoQuery) Execute(client *Client) (NetworkVersionInfo, error) {
+	if client == nil || client.operator == nil {
+		return NetworkVersionInfo{}, errNoClientProvided
+	}
+
+	query.queryPayment = NewHbar(2)
+	query.paymentTransactionID = TransactionIDGenerate(client.operator.accountID)
+
+	var cost Hbar
+	if query.queryPayment.tinybar != 0 {
+		cost = query.queryPayment
+	} else {
+		cost = client.maxQueryPayment
+
+		// actualCost := CostQuery()
+	}
+
+	err := query_generatePayments(&query.Query, client, cost)
+	if err != nil {
+		return NetworkVersionInfo{}, err
+	}
+
+
+	resp, err := execute(
+		client,
+		request{
+			query: &query.Query,
+		},
+		query_shouldRetry,
+		query_makeRequest,
+		query_advanceRequest,
+		query_getNodeId,
+		networkVersionInfoQuery_getMethod,
+		networkVersionInfoQuery_mapResponseStatus,
+		query_mapResponse,
+	)
+
+	if err != nil {
+		return NetworkVersionInfo{}, err
+	}
+
+	return networkVersionInfoFromProtobuf(resp.query.GetNetworkGetVersionInfo()), err
+}
+
+// SetMaxQueryPayment sets the maximum payment allowed for this Query.
+func (query *NetworkVersionInfoQuery) SetMaxQueryPayment(maxPayment Hbar) *NetworkVersionInfoQuery {
+	query.Query.SetMaxQueryPayment(maxPayment)
+	return query
+}
+
+// SetQueryPayment sets the payment amount for this Query.
+func (query *NetworkVersionInfoQuery) SetQueryPayment(paymentAmount Hbar) *NetworkVersionInfoQuery {
+	query.Query.SetQueryPayment(paymentAmount)
+	return query
+}
+
+func (query *NetworkVersionInfoQuery) SetNodeAccountID(accountID AccountID) *NetworkVersionInfoQuery {
+	query.Query.SetNodeAccountID(accountID)
+	return query
+}
+
+func (query *NetworkVersionInfoQuery) GetNodeAccountId() AccountID {
+	return query.Query.GetNodeAccountId()
+}
