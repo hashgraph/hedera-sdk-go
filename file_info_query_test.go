d1baae228d247c43015dd20fc8e4399bc1123c4a
Andrei Kuzmiankov <ender2016@gmail.com>
2020-10-19 20:10:53 -0700

    feat: Added file_info_query_test

---
+++ file_info_query_test.go
@@ -0,0 +1,65 @@
+package hedera
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+
+func TestFileInfoQueryTransaction_Execute(t *testing.T) {
+	client, err := ClientFromJsonFile(os.Getenv("CONFIG_FILE"))
+
+	if err != nil {
+		client = ClientForTestnet()
+	}
+
+	configOperatorID := os.Getenv("OPERATOR_ID")
+	configOperatorKey := os.Getenv("OPERATOR_KEY")
+
+	if configOperatorID != "" && configOperatorKey != "" {
+		operatorAccountID, err := AccountIDFromString(configOperatorID)
+		assert.NoError(t, err)
+
+		operatorKey, err := PrivateKeyFromString(configOperatorKey)
+		assert.NoError(t, err)
+
+		client.SetOperator(operatorAccountID, operatorKey)
+	}
+
+	client.SetMaxTransactionFee(NewHbar(2))
+
+	resp, err := NewFileCreateTransaction().
+		SetKeys(client.GetOperatorKey()).
+		SetContents([]byte("Hello, World")).
+		SetTransactionMemo("go sdk e2e tests").
+		Execute(client)
+
+	assert.NoError(t, err)
+
+	receipt, err := resp.TransactionID.GetReceipt(client)
+	assert.NoError(t, err)
+
+	fileID := receipt.FileID
+	assert.NotNil(t, fileID)
+
+	info, err := NewFileInfoQuery().
+		SetFileID(*fileID).
+		SetNodeAccountID(resp.NodeID).
+		SetQueryPayment(NewHbar(22)).
+		Execute(client)
+	assert.NoError(t, err)
+
+	assert.Equal(t, *fileID, info.FileID)
+	assert.Equal(t, info.Size, 28)
+	assert.False(t, info.IsDeleted)
+
+	resp, err = NewFileDeleteTransaction().
+		SetFileID(*fileID).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_, err = resp.TransactionID.GetReceipt(client)
+	assert.NoError(t, err)
+}
+
