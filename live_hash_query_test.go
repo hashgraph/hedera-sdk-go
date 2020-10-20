5c699c0957851a168d39d637d71620c8244d126e
Andrei Kuzmiankov <ender2016@gmail.com>
2020-10-19 20:11:09 -0700

    feat: Added live_hash_query_test

---
+++ live_hash_query_test.go
@@ -0,0 +1,85 @@
+package hedera
+
+import (
+	"encoding/hex"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestLiveHashQuery_Execute(t *testing.T) {
+	client, err := ClientFromJsonFile(os.Getenv("CONFIG_FILE"))
+
+	if err != nil {
+		client = ClientForTestnet()
+	}
+
+	_hash, err := hex.DecodeString("100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002")
+	if err != nil {
+		println("Decode Failed.")
+	}
+
+	configOperatorID := os.Getenv("OPERATOR_ID")
+	configOperatorKey := os.Getenv("OPERATOR_KEY")
+
+	if configOperatorID != "" && configOperatorKey != "" {
+		operatorAccountID, err := AccountIDFromString(configOperatorID)
+		assert.NoError(t, err)
+
+		operatorKey, err := PrivateKeyFromString(configOperatorKey)
+		assert.NoError(t, err)
+
+		client.SetOperator(operatorAccountID, operatorKey)
+	}
+
+	newKey, err := GeneratePrivateKey()
+	assert.NoError(t, err)
+
+	resp, err := NewAccountCreateTransaction().
+		SetKey(newKey.PublicKey()).
+		SetMaxTransactionFee(NewHbar(2)).
+		SetInitialBalance(NewHbar(1)).
+		SetNodeAccountID(AccountID{Account: 3}).
+		Execute(client)
+
+	receipt, err := resp.GetReceipt(client)
+	assert.NoError(t, err)
+
+	accountID := *receipt.AccountID
+
+	println("NodeID", resp.NodeID.String())
+	resp, err = NewLiveHashAddTransaction().
+		SetAccountID(accountID).
+		SetDuration(24 * 30 * time.Hour).
+		SetNodeAccountID(resp.NodeID).
+		SetHash(_hash).
+		SetKeys(newKey.PublicKey()).
+		Execute(client)
+
+	assert.NoError(t, err)
+
+	println("TransactionID", resp.TransactionID.String())
+	println("NodeID", resp.NodeID.String())
+
+	_, err = NewLiveHashDeleteTransaction().
+		SetAccountID(accountID).
+		SetNodeAccountID(resp.NodeID).
+		SetHash(_hash).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_, err = NewLiveHashQuery().
+		SetAccountID(accountID).
+		SetNodeAccountID(resp.NodeID).
+		SetHash(_hash).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_,err = NewAccountDeleteTransaction().
+		SetAccountID(accountID).
+		SetNodeAccountID(resp.NodeID).
+		SetTransferAccountID(client.GetOperatorID()).
+		Execute(client)
+	assert.NoError(t, err)
+}
