646f8742d9cd9533412e57f226e7a2bcdeed19dd
Andrei Kuzmiankov <ender2016@gmail.com>
2020-10-19 20:11:25 -0700

    feat: Added network_version_info_query_test

---
+++ network_version_info_query_test.go
@@ -0,0 +1,35 @@
+package hedera
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestNetworkVersionInfoQuery_Execute(t *testing.T) {
+	client, err := ClientFromJsonFile(os.Getenv("CONFIG_FILE"))
+
+	if err != nil {
+		client = ClientForTestnet()
+	}
+
+	if err != nil {
+		println("Decode Failed.")
+	}
+
+	configOperatorID := os.Getenv("OPERATOR_ID")
+	configOperatorKey := os.Getenv("OPERATOR_KEY")
+
+	if configOperatorID != "" && configOperatorKey != "" {
+		operatorAccountID, err := AccountIDFromString(configOperatorID)
+		assert.NoError(t, err)
+
+		operatorKey, err := PrivateKeyFromString(configOperatorKey)
+		assert.NoError(t, err)
+
+		client.SetOperator(operatorAccountID, operatorKey)
+	}
+
+	_, err = NewNetworkVersionQuery().Execute(client)
+	assert.NoError(t, err)
+}
