d8663f93cffd3a0808b9e6cf11c0a1a4c0760523
Andrei Kuzmiankov <ender2016@gmail.com>
2020-10-20 17:33:58 -0700

    fix: fixed token_info kyc|freezefromprotobuf

--- token_info.go
+++ token_info.go
@@ -1,5 +1,7 @@
 package hedera
 
+import "github.com/hashgraph/hedera-sdk-go/proto"
+
 type TokenInfo struct {
 	TokenID             TokenID
 	Name                string
@@ -12,55 +14,68 @@ type TokenInfo struct {
 	FreezeKey           Key
 	WipeKey             Key
 	SupplyKey           Key
-	DefaultFreezeStatus bool
-	DefaultKycStatus    bool
+	DefaultFreezeStatus *bool
+	DefaultKycStatus    *bool
 	IsDelete            bool
 	AutoRenewPeriod     uint64
 	ExpirationTime      uint64
 }
 
-//func freezeStatusFromProtobuf(freezeStatus TokenFreezeStatus) bool {
-//	if freezeStatus.Number() == 1 {
-//		return true
-//	}
-//	return false
-//}
-//
-//func kycStatusFromProtobuf(kycStatus TokenKycStatus) bool {
-//	if kycStatus.Number() == 1 {
-//		return true
-//	}
-//	return false
-//}
-//
-//func tokenInfoFromProtobuf(tokenInfoResponse *proto.TokenGetInfoResponse) TokenInfo {
-//	tokenInfo := tokenInfoResponse.TokenInfo
-//	return TokenInfo{
-//		TokenID:     tokenIDFromProtobuf(tokenInfo.TokenId),
-//		Name:        tokenInfo.Name,
-//		Symbol:      tokenInfo.Symbol,
-//		Decimals:    tokenInfo.Decimals,
-//		TotalSupply: tokenInfo.TotalSupply,
-//		Treasury:    accountIDFromProtobuf(tokenInfo.Treasury),
-//		AdminKey: PublicKey{
-//			keyData: tokenInfo.AdminKey.GetEd25519(),
-//		},
-//		KycKey: PublicKey{
-//			keyData: tokenInfo.KycKey.GetEd25519(),
-//		},
-//		FreezeKey: PublicKey{
-//			keyData: tokenInfo.FreezeKey.GetEd25519(),
-//		},
-//		WipeKey: PublicKey{
-//			keyData: tokenInfo.WipeKey.GetEd25519(),
-//		},
-//		SupplyKey: PublicKey{
-//			keyData: tokenInfo.SupplyKey.GetEd25519(),
-//		},
-//		DefaultFreezeStatus: freezeStatusFromProtobuf(tokenInfo.DefaultFreezeStatus),
-//		DefaultKycStatus:    kycStatusFromProtobuf(tokenInfo.DefaultKycStatus),
-//		IsDelete:            tokenInfo.IsDeleted,
-//		AutoRenewPeriod:     tokenInfo.AutoRenewPeriod,
-//		ExpirationTime:      tokenInfo.Expiry,
-//	}
-//}
+func freezeStatusFromProtobuf(pb *proto.TokenFreezeStatus) *bool {
+	var freezeStatus *bool
+	switch pb.Number() {
+	case 1:
+		*freezeStatus = true
+	case 2:
+		*freezeStatus = false
+	default:
+		freezeStatus = nil
+	}
+
+	return freezeStatus
+}
+
+func kycStatusFromProtobuf(pb *proto.TokenKycStatus) *bool {
+	var kycStatus *bool
+	switch pb.Number() {
+	case 1:
+		*kycStatus = true
+	case 2:
+		*kycStatus = false
+	default:
+		kycStatus = nil
+	}
+	return kycStatus
+}
+
+func tokenInfoFromProtobuf(tokenInfoResponse *proto.TokenGetInfoResponse) TokenInfo {
+	tokenInfo := tokenInfoResponse.TokenInfo
+	return TokenInfo{
+		TokenID:     tokenIDFromProtobuf(tokenInfo.TokenId),
+		Name:        tokenInfo.Name,
+		Symbol:      tokenInfo.Symbol,
+		Decimals:    tokenInfo.Decimals,
+		TotalSupply: tokenInfo.TotalSupply,
+		Treasury:    accountIDFromProtobuf(tokenInfo.Treasury),
+		AdminKey: PublicKey{
+			keyData: tokenInfo.AdminKey.GetEd25519(),
+		},
+		KycKey: PublicKey{
+			keyData: tokenInfo.KycKey.GetEd25519(),
+		},
+		FreezeKey: PublicKey{
+			keyData: tokenInfo.FreezeKey.GetEd25519(),
+		},
+		WipeKey: PublicKey{
+			keyData: tokenInfo.WipeKey.GetEd25519(),
+		},
+		SupplyKey: PublicKey{
+			keyData: tokenInfo.SupplyKey.GetEd25519(),
+		},
+		DefaultFreezeStatus: freezeStatusFromProtobuf(&tokenInfo.DefaultFreezeStatus),
+		DefaultKycStatus:    kycStatusFromProtobuf(&tokenInfo.DefaultKycStatus),
+		IsDelete:            tokenInfo.IsDeleted,
+		AutoRenewPeriod:     tokenInfo.AutoRenewPeriod,
+		ExpirationTime:      tokenInfo.Expiry,
+	}
+}
