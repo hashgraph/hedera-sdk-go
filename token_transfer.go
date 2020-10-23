2664096233a0a220d28dcb37f5abe91392377173
Andrei Kuzmiankov <ender2016@gmail.com>
2020-10-23 05:13:44 -0700

    feat: Added token_transfer

---
+++ token_transfer.go
@@ -0,0 +1,29 @@
+package hedera
+
+import "github.com/hashgraph/hedera-sdk-go/proto"
+
+type TokenTransfer struct {
+	AccountID AccountID
+	Amount int64
+}
+
+func NewTokenTransfer(accountID AccountID, amount int64) TokenTransfer {
+	return TokenTransfer{
+		AccountID: accountID,
+		Amount: amount,
+	}
+}
+
+func tokenTransferFromProtobuf(pb *proto.AccountAmount) TokenTransfer {
+	return TokenTransfer{
+		AccountID: accountIDFromProtobuf(pb.AccountID),
+		Amount:    pb.Amount,
+	}
+}
+
+func (transfer *TokenTransfer) toProtobuf() *proto.AccountAmount {
+	return &proto.AccountAmount{
+		AccountID: transfer.AccountID.toProtobuf(),
+		Amount:    transfer.Amount,
+	}
+}
