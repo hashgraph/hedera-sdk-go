4fb60996737a7129bcce2ac0b0cb4584cc73632b
Andrei Kuzmiankov <ender2016@gmail.com>
2020-10-29 05:01:35 -0700

    feat: Added token_revoke_kyc_transaction_test

---
+++ token_revoke_kyc_transaction_test.go
@@ -0,0 +1,141 @@
+package hedera
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestTokenRevokeKycTransaction_Execute(t *testing.T) {
+	var client *Client
+
+	network := os.Getenv("HEDERA_NETWORK")
+
+	if network == "previewnet" {
+		client = ClientForPreviewnet()
+	}
+
+	var err error
+	client, err = ClientFromJsonFile(os.Getenv("CONFIG_FILE"))
+
+	if err != nil {
+		client = ClientForTestnet()
+	}
+
+	client.SetMirrorNetwork([]string{"hcs.previewnet.mirrornode.hedera.com:5600"})
+
+	configOperatorID := os.Getenv("OPERATOR_ID")
+	configOperatorKey := os.Getenv("OPERATOR_KEY")
+	var operatorKey PrivateKey
+	var operatorAccountID AccountID
+
+	if configOperatorID != "" && configOperatorKey != "" {
+		operatorAccountID, err = AccountIDFromString(configOperatorID)
+		assert.NoError(t, err)
+
+		operatorKey, err = PrivateKeyFromString(configOperatorKey)
+		assert.NoError(t, err)
+
+		client.SetOperator(operatorAccountID, operatorKey)
+	}
+
+	newKey, err := GeneratePrivateKey()
+	assert.NoError(t, err)
+
+	newBalance := NewHbar(1)
+
+	assert.Equal(t, HbarUnits.Hbar.numberOfTinybar(), newBalance.tinybar)
+
+	resp, err := NewAccountCreateTransaction().
+		SetKey(newKey.PublicKey()).
+		SetMaxTransactionFee(NewHbar(2)).
+		SetInitialBalance(newBalance).
+		Execute(client)
+	assert.NoError(t, err)
+
+	receipt, err := resp.GetReceipt(client)
+	assert.NoError(t, err)
+
+	accountID := *receipt.AccountID
+
+	resp, err = NewTokenCreateTransaction().
+		SetName("ffff").
+		SetSymbol("F").
+		SetDecimals(3).
+		SetInitialSupply(1000000).
+		SetTreasury(operatorAccountID).
+		SetAdminKey(operatorKey.PublicKey()).
+		SetFreezeKey(operatorKey.PublicKey()).
+		SetWipeKey(operatorKey.PublicKey()).
+		SetKycKey(operatorKey.PublicKey()).
+		SetSupplyKey(operatorKey.PublicKey()).
+		SetFreezeDefault(false).
+		SetExpirationTime(uint64(time.Now().Unix() + 86400*90)).
+		Execute(client)
+	assert.NoError(t, err)
+
+	receipt, err = resp.GetReceipt(client)
+	assert.NoError(t, err)
+
+	tokenID := *receipt.TokenID
+
+	transaction, err := NewTokenAssociateTransaction().
+		SetNodeAccountIDs([]AccountID{resp.NodeID}).
+		SetAccountID(accountID).
+		SetTokenIDs(tokenID).
+		FreezeWith(client)
+	assert.NoError(t, err)
+
+	resp, err = transaction.
+		Sign(operatorKey).
+		Sign(newKey).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_, err = resp.GetReceipt(client)
+	assert.NoError(t, err)
+
+	resp, err = NewTokenGrantKycTransaction().
+		SetNodeAccountIDs([]AccountID{resp.NodeID}).
+		SetAccountID(accountID).
+		SetTokenID(tokenID).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_, err = resp.GetReceipt(client)
+	assert.NoError(t, err)
+
+	resp, err = NewTokenRevokeKycTransaction().
+		SetNodeAccountIDs([]AccountID{resp.NodeID}).
+		SetAccountID(accountID).
+		SetTokenID(tokenID).
+		Execute(client)
+
+	_, err = resp.GetReceipt(client)
+	assert.NoError(t, err)
+
+	resp, err = NewTokenDeleteTransaction().
+		SetNodeAccountIDs([]AccountID{resp.NodeID}).
+		SetTokenID(tokenID).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_, err = resp.GetReceipt(client)
+	assert.NoError(t, err)
+
+	tx, err := NewAccountDeleteTransaction().
+		SetAccountID(accountID).
+		SetTransferAccountID(client.GetOperatorID()).
+		FreezeWith(client)
+	assert.NoError(t, err)
+
+	resp, err = tx.
+		Sign(operatorKey).
+		Sign(newKey).
+		Execute(client)
+	assert.NoError(t, err)
+
+	_, err = resp.GetReceipt(client)
+	assert.NoError(t, err)
+}
