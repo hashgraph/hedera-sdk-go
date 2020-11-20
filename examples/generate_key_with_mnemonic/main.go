3ea1d20c51c8a8928750548095ae21976167b9db
Andrei Kuzmiankov <ender2016@gmail.com>
2020-11-19 20:01:00 -0800

    feat: Added legacy mnemonic support

--- examples/generate_key_with_mnemonic/main.go
+++ examples/generate_key_with_mnemonic/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/hashgraph/hedera-sdk-go"
 )
@@ -11,13 +12,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	mnemonic12, err := hedera.GenerateMnemonic12()
+	if err != nil {
+		panic(err)
+	}
+
+	s := "jolly,kidnap,tom,lawn,drunk,chick,optic,lust,mutter,mole,bride,galley,dense,member,sage,neural,widow,decide,curb,aboard,margin,manure"
+
+	mnemonicleg, err := hedera.NewMnemonic(strings.Split(s, ","))
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("mnemonic 24 word = %v\n", mnemonic24)
 	fmt.Printf("mnemonic 12 word = %v\n", mnemonic12)
+	fmt.Printf("mnemonic 12 word = %v\n", mnemonicleg)
 
 	privateKey24, err := mnemonic24.ToPrivateKey( /* passphrase */ "")
 	if err != nil {
@@ -27,13 +37,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	privateleg, err := mnemonicleg.ToLegacyPrivateKey()
+	if err != nil {
+		panic(err)
+	}
 
 	publicKey24 := privateKey24.PublicKey()
 	publicKey12 := privateKey12.PublicKey()
+	publicleg := privateleg.PublicKey()
 
 	fmt.Printf("private 24 word = %v\n", privateKey24)
 	fmt.Printf("public 24 word = %v\n", publicKey24)
 
 	fmt.Printf("private 12 word = %v\n", privateKey12)
 	fmt.Printf("public 12 word = %v\n", publicKey12)
+
+	fmt.Printf("private legacy word = %v\n", privateleg)
+	fmt.Printf("public legacy word = %v\n", publicleg)
 }
--- mnemonic.go
+++ mnemonic.go
@@ -1,8 +1,11 @@
 package hedera
 
 import (
+	"crypto/sha512"
 	"fmt"
 	"github.com/tyler-smith/go-bip39"
+	"golang.org/x/crypto/pbkdf2"
+	"math/big"
 	"strings"
 )
 
@@ -72,11 +75,123 @@ func (m Mnemonic) Words() []string {
 //
 // Keys are lazily generated
 func NewMnemonic(words []string) (Mnemonic, error) {
-	if len(words) == 24 || len(words) == 12 {
+	if len(words) == 24 || len(words) == 12 || len(words) == 22 {
 		return Mnemonic{
 			words: strings.Join(words, " "),
 		}, nil
 	} else {
 		return Mnemonic{}, fmt.Errorf("invalid mnemonic string")
 	}
+}
+
+func (m Mnemonic) legacyValidate() (Mnemonic, error) {
+	if len(m.words) != 22 {
+		return Mnemonic{}, fmt.Errorf("not a legacy mnemonic")
+	}
+
+	indices, err := m.indices()
+	if err != nil {
+		return Mnemonic{}, err
+	}
+
+	entropy, checksum := m.toLegacyEntropy(indices)
+	newChecksum := crc8(entropy)
+
+	if checksum != newChecksum {
+		return Mnemonic{}, fmt.Errorf("legacy mnemonic checksum mismatch")
+	}
+
+	return m, nil
+}
+
+func (m Mnemonic) indices() ([]int, error) {
+	var indices []int
+	var check bool
+	for _, mnemonicString := range strings.Split(m.words, " ") {
+		check = false
+		for i, stringCheck := range legacy {
+			if mnemonicString == stringCheck {
+				check = true
+				indices = append(indices, int(i))
+			}
+		}
+		if !check {
+			return make([]int, 0), fmt.Errorf("word is not in the legacy word list")
+		}
+	}
+
+	return indices, nil
+}
+
+func (m Mnemonic) ToLegacyPrivateKey() (PrivateKey, error) {
+	indices, err := m.indices()
+	if err != nil {
+		return PrivateKey{}, err
+	}
+
+	entropy, _ := m.toLegacyEntropy(indices)
+	password := make([]uint8, len(entropy)+8)
+	for i, number := range entropy {
+		password[i] = number
+	}
+	for i := len(entropy); i<len(password); i++ {
+		password[i] = 0xFF
+	}
+	salt := []byte{0xFF}
+
+	keyData := pbkdf2.Key(password, salt, 2048, 32, sha512.New)
+
+	return PrivateKeyFromBytes(keyData)
+}
+
+func (m Mnemonic) toLegacyEntropy(indices []int) ([]byte, uint8) {
+	data := convertRadix(indices, len(legacy), 256, 33)
+	checksum := data[len(data)-1]
+	result := make([]uint8, len(data)-1)
+
+	for i := 0; i < len(data)-1; i++ {
+		result[i] = data[i] ^ checksum
+	}
+
+	return result, checksum
+}
+
+func convertRadix(nums []int, fromRadix int, toRadix int, toLength int) []uint8 {
+	num := big.NewInt(0)
+
+	for _, element := range nums {
+		num = num.Mul(num, big.NewInt(int64(fromRadix)))
+		num = num.Add(num, big.NewInt(int64(element)))
+	}
+
+	result := make([]uint8, toLength)
+
+	for i := toLength - 1; i >= 0; i -= 1 {
+		tem := num.Div(num, big.NewInt(int64(toRadix)))
+		rem := num.Mod(num, big.NewInt(int64(toRadix)))
+		num = tem
+		result[i] = uint8(rem.Uint64())
+	}
+
+	return result
+}
+
+func crc8(data []uint8) uint8 {
+	var crc uint8
+	crc = 0xff
+
+	for i := 0; i < len(data)-1; i++ {
+		crc ^= data[i]
+		for j := 0; j < 8; j++ {
+			var temp uint8
+			if crc&1 == 0 {
+				temp = 0
+			} else {
+				temp = 0xb2
+			}
+			crc = crc>>1 ^ temp
+		}
+	}
+
+	return crc ^ 0xff
 }
