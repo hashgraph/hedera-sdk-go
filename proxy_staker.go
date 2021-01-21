b5a30a1165a6b57505a7b5ce278b3c82b16ba655
Andrei Kuzmiankov <andrei@launchbadge.com>
2021-01-21 15:24:05 -0800

    feat: Added proxy_staker

---
+++ proxy_staker.go
@@ -0,0 +1,31 @@
+package hedera
+
+import (
+	"github.com/hashgraph/hedera-sdk-go/v2/proto"
+)
+
+type ProxyStaker struct {
+	AccountID AccountID
+	amount    Hbar
+}
+
+func newProxyStaker(accountId AccountID, amount int64) ProxyStaker {
+	return ProxyStaker{
+		AccountID: accountId,
+		amount:    HbarFromTinybar(amount),
+	}
+}
+
+func fromProtobuf(staker *proto.ProxyStaker) ProxyStaker {
+	return ProxyStaker{
+		AccountID: accountIDFromProtobuf(staker.AccountID),
+		amount:    HbarFromTinybar(staker.Amount),
+	}
+}
+
+func (staker *ProxyStaker) toProtobuf() *proto.ProxyStaker {
+	return &proto.ProxyStaker{
+		AccountID: staker.AccountID.toProtobuf(),
+		Amount:    staker.amount.tinybar,
+	}
+}
