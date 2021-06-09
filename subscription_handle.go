b960f9ae7bfb2c926c591fc979bdc654f46c4a90
Daniel Akhterov <daniel@launchbadge.com>
2021-06-08 17:33:39 -0700

    refactor: `TopicMessageQuery`
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- subscription_handle.go
+++ subscription_handle.go
@@ -4,10 +4,8 @@ type SubscriptionHandle struct {
 	onUnsubscribe func()
 }
 
-func newSubscriptionHandle(onUnsubscribe func()) SubscriptionHandle {
-	return SubscriptionHandle{onUnsubscribe: onUnsubscribe}
-}
-
 func (handle SubscriptionHandle) Unsubscribe() {
-	handle.onUnsubscribe()
+	if handle.onUnsubscribe != nil {
+		handle.onUnsubscribe()
+	}
 }
