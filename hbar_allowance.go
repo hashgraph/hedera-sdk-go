9e4aad11e3acc62ed256460df64a373db00538ad
Daniel Akhterov <daniel@launchbadge.com>
2022-01-30 22:21:01 -0800

    fix: stashed changes accidently making into history
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

--- hbar_allowance.go
+++ hbar_allowance.go
@@ -1,62 +1,3 @@
-<<<<<<< Updated upstream
-package hedera
-
-import "github.com/hashgraph/hedera-protobufs-go/services"
-
-type HbarAllowance struct {
-	SpenderAccountID *AccountID
-	Amount           int64
-}
-
-func NewHbarAllowance(id AccountID, amount int64) HbarAllowance {
-	return HbarAllowance{
-		SpenderAccountID: &id,
-		Amount:           amount,
-	}
-}
-
-func (approval *HbarAllowance) SetSpender(id AccountID) *HbarAllowance {
-	approval.SpenderAccountID = &id
-	return approval
-}
-
-func (approval *HbarAllowance) GetSpender() AccountID {
-	if approval.SpenderAccountID != nil {
-		return *approval.SpenderAccountID
-	}
-
-	return AccountID{}
-}
-
-func (approval *HbarAllowance) SetAmount(amount int64) *HbarAllowance {
-	approval.Amount = amount
-	return approval
-}
-
-func (approval *HbarAllowance) GetAmount() int64 {
-	return approval.Amount
-}
-
-func _HbarAllowanceFromProtobuf(pb *services.CryptoAllowance) HbarAllowance {
-	return HbarAllowance{
-		SpenderAccountID: _AccountIDFromProtobuf(pb.Spender),
-		Amount:           pb.Amount,
-	}
-}
-
-func (approval *HbarAllowance) _ToProtobuf() *services.CryptoAllowance {
-	body := &services.CryptoAllowance{
-		Amount: approval.Amount,
-	}
-
-	if approval.SpenderAccountID != nil {
-		body.Spender = approval.SpenderAccountID._ToProtobuf()
-	}
-
-	return body
-}
-||||||| constructed merge base
-=======
 package hedera
 
 import "github.com/hashgraph/hedera-protobufs-go/services"
@@ -113,4 +54,3 @@ func (approval *HbarAllowance) _ToProtobuf() *services.CryptoAllowance {
 
 	return body
 }
->>>>>>> Stashed changes
