b2713b72bbf1c451f692c3a2aa9620c56c17cd5e
Andrei <andrei@launchbadge.com>
2022-03-21 20:31:12 -0700

    feat: added support for uint24 and int24 for solidity (#424)
    * feat: added support for uint24 and int24 for solidity
    
    Signed-off-by: Andrei Kuzmiankov <andrei@launchbadge.com>
    
    * feat: support all integer widths for ContractFunction[Parameters|Selector|Result]
    
    Signed-off-by: Andrei Kuzmiankov <andrei@launchbadge.com>

--- contract_function_selector.go
+++ contract_function_selector.go
@@ -21,12 +21,68 @@ const (
 	aBool     argument = "bool"
 	aString   argument = "string"
 	aInt8     argument = "int8"
+	aInt16    argument = "int16"
+	aInt24    argument = "int24"
 	aInt32    argument = "int32"
+	aInt40    argument = "int40"
+	aInt48    argument = "int48"
+	aInt56    argument = "int56"
 	aInt64    argument = "int64"
+	aInt72    argument = "int72"
+	aInt80    argument = "int80"
+	aInt88    argument = "int88"
+	aInt96    argument = "int96"
+	aInt104   argument = "int104"
+	aInt112   argument = "int112"
+	aInt120   argument = "int120"
+	aInt128   argument = "int128"
+	aInt136   argument = "int136"
+	aInt144   argument = "int144"
+	aInt152   argument = "int152"
+	aInt160   argument = "int160"
+	aInt168   argument = "int168"
+	aInt176   argument = "int176"
+	aInt184   argument = "int184"
+	aInt192   argument = "int192"
+	aInt200   argument = "int200"
+	aInt208   argument = "int208"
+	aInt216   argument = "int216"
+	aInt224   argument = "int224"
+	aInt232   argument = "int232"
+	aInt240   argument = "int240"
+	aInt248   argument = "int248"
 	aInt256   argument = "int256"
 	aUint8    argument = "uint8"
+	aUint16   argument = "uint16"
+	aUint24   argument = "uint24"
 	aUint32   argument = "uint32"
+	aUint40   argument = "uint40"
+	aUint48   argument = "uint48"
+	aUint56   argument = "uint56"
 	aUint64   argument = "uint64"
+	aUint72   argument = "uint72"
+	aUint80   argument = "uint80"
+	aUint88   argument = "uint88"
+	aUint96   argument = "uint96"
+	aUint104  argument = "uint104"
+	aUint112  argument = "uint112"
+	aUint120  argument = "uint120"
+	aUint128  argument = "uint128"
+	aUint136  argument = "uint136"
+	aUint144  argument = "uint144"
+	aUint152  argument = "uint152"
+	aUint160  argument = "uint160"
+	aUint168  argument = "uint168"
+	aUint176  argument = "uint176"
+	aUint184  argument = "uint184"
+	aUint192  argument = "uint192"
+	aUint200  argument = "uint200"
+	aUint208  argument = "uint208"
+	aUint216  argument = "uint216"
+	aUint224  argument = "uint224"
+	aUint232  argument = "uint232"
+	aUint240  argument = "uint240"
+	aUint248  argument = "uint248"
 	aUint256  argument = "uint256"
 	aBytes    argument = "bytes"
 	aBytes32  argument = "bytes32"
@@ -99,6 +155,20 @@ func (selector *ContractFunctionSelector) AddInt8() *ContractFunctionSelector {
 	})
 }
 
+func (selector *ContractFunctionSelector) AddInt16() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt16,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt24() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt24,
+		array: false,
+	})
+}
+
 func (selector *ContractFunctionSelector) AddInt32() *ContractFunctionSelector {
 	return selector._AddParam(_Solidity{
 		ty:    aInt32,
@@ -106,9 +176,181 @@ func (selector *ContractFunctionSelector) AddInt32() *ContractFunctionSelector {
 	})
 }
 
+func (selector *ContractFunctionSelector) AddInt40() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt40,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt48() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt48,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt56() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt56,
+		array: false,
+	})
+}
+
 func (selector *ContractFunctionSelector) AddInt64() *ContractFunctionSelector {
 	return selector._AddParam(_Solidity{
 		ty:    aInt64,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt72() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt72,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt80() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt80,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt88() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt88,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddInt96() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt96,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddInt104() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt104,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddInt112() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt112,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddInt120() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt120,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddInt128() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt128,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddInt136() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt136,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddInt144() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt144,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddInt152() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt152,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddInt160() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt160,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddInt168() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt168,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt176() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt176,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt184() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt184,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt192() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt192,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt200() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt200,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt208() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt208,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt216() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt216,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt224() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt224,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt232() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt232,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt240() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt240,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddInt248() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aInt248,
 		array: false,
 	})
 }
@@ -127,6 +369,20 @@ func (selector *ContractFunctionSelector) AddUint8() *ContractFunctionSelector {
 	})
 }
 
+func (selector *ContractFunctionSelector) AddUint16() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint16,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint24() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint24,
+		array: false,
+	})
+}
+
 func (selector *ContractFunctionSelector) AddUint32() *ContractFunctionSelector {
 	return selector._AddParam(_Solidity{
 		ty:    aUint32,
@@ -134,9 +390,181 @@ func (selector *ContractFunctionSelector) AddUint32() *ContractFunctionSelector 
 	})
 }
 
+func (selector *ContractFunctionSelector) AddUint40() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint40,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint48() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint48,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint56() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint56,
+		array: false,
+	})
+}
+
 func (selector *ContractFunctionSelector) AddUint64() *ContractFunctionSelector {
 	return selector._AddParam(_Solidity{
 		ty:    aUint64,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint72() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint72,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint80() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint80,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint88() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint88,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddUint96() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint96,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddUint104() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint104,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddUint112() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint112,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddUint120() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint120,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddUint128() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint128,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddUint136() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint136,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddUint144() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint144,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddUint152() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint152,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddUint160() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint160,
+		array: false,
+	})
+}
+func (selector *ContractFunctionSelector) AddUint168() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint168,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint176() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint176,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint184() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint184,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint192() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint192,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint200() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint200,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint208() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint208,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint216() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint216,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint224() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint224,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint232() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint232,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint240() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint240,
+		array: false,
+	})
+}
+
+func (selector *ContractFunctionSelector) AddUint248() *ContractFunctionSelector {
+	return selector._AddParam(_Solidity{
+		ty:    aUint248,
 		array: false,
 	})
 }
