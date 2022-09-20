27b1aaaddf542bc296786b66b70303c13ffc61cd
Andrei <andrei@launchbadge.com>
2022-09-19 20:54:29 -0700

    feat: adding node auto updating to client (#572)
    Signed-off-by: Andrei Kuzmiankov <andrei@launchbadge.com>
    
    Signed-off-by: Andrei Kuzmiankov <andrei@launchbadge.com>

--- account_create_transaction.go
+++ account_create_transaction.go
@@ -61,7 +61,7 @@ func NewAccountCreateTransaction() *AccountCreateTransaction {
 	}
 
 	transaction.SetAutoRenewPeriod(7890000 * time.Second)
-	transaction.SetMaxTransactionFee(NewHbar(3))
+	transaction.SetMaxTransactionFee(NewHbar(5))
 
 	return &transaction
 }
