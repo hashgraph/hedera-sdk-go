4b71f2e02b5a6b6ecbfbb26740e853aa252dc54d
Emanuel Pargov <bamzedev@gmail.com>
2022-10-26 18:14:28 +0300

    Add example for HIP-542
    Signed-off-by: Emanuel Pargov <bamzedev@gmail.com>

---
+++ examples/account_create_with_hts/main.go
@@ -0,0 +1,300 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hashgraph/hedera-sdk-go/v2"
+	"os"
+)
+
+func main() {
+
+	client, err := hedera.ClientForName(os.Getenv("HEDERA_NETWORK"))
+	if err != nil {
+		println(err.Error(), ": error creating client")
+		return
+	}
+
+	operatorId, err := hedera.AccountIDFromString(os.Getenv("OPERATOR_ID"))
+	if err != nil {
+		println(err.Error(), ": error converting string to AccountID")
+		return
+	}
+
+	// Retrieving operator key from environment variable OPERATOR_KEY
+	operatorKey, err := hedera.PrivateKeyFromStringEd25519(os.Getenv("OPERATOR_KEY"))
+	if err != nil {
+		println(err.Error(), ": error converting string to PrivateKey")
+		return
+	}
+
+	// Setting the client operator ID and key
+	client.SetOperator(operatorId, operatorKey)
+
+	supplyKey, err := hedera.PrivateKeyGenerateEcdsa()
+	if err != nil {
+		println(err.Error(), ": error creating supply key")
+		return
+	}
+	freezeKey, err := hedera.PrivateKeyGenerateEcdsa()
+	if err != nil {
+		println(err.Error(), ": error creating freeze key")
+		return
+	}
+	wipeKey, err := hedera.PrivateKeyGenerateEcdsa()
+	if err != nil {
+		println(err.Error(), ": error creating wipe key")
+		return
+	}
+	/**
+	 *     Example 1
+	 *
+	 * Step 1
+	 *
+	 * Create an NFT using the Hedera Token Service
+	 */
+	fmt.Println("Example 1")
+	// IPFS content identifiers for the NFT metadata
+	cid := []string{"QmNPCiNA3Dsu3K5FxDPMG5Q3fZRwVTg14EXA92uqEeSRXn",
+		"QmZ4dgAgt8owvnULxnKxNe8YqpavtVCXmc1Lt2XajFpJs9",
+		"QmPzY5GxevjyfMUF5vEAjtyRoigzWp47MiKAtLBduLMC1T",
+		"Qmd3kGgSrAwwSrhesYcY7K54f3qD7MDo38r7Po2dChtQx5",
+		"QmWgkKz3ozgqtnvbCLeh7EaR1H8u5Sshx3ZJzxkcrT3jbw"}
+	// Creating the transaction for token creation
+	nftCreateTransaction, err := hedera.NewTokenCreateTransaction().
+		SetTokenName("HIP-542 Example Collection").SetTokenSymbol("HIP-542").
+		SetTokenType(hedera.TokenTypeNonFungibleUnique).SetDecimals(0).
+		SetInitialSupply(0).SetMaxSupply(int64(len(cid))).
+		SetTreasuryAccountID(operatorId).SetSupplyType(hedera.TokenSupplyTypeFinite).
+		SetAdminKey(operatorKey).SetFreezeKey(freezeKey).SetWipeKey(wipeKey).SetSupplyKey(supplyKey).FreezeWith(client)
+	if err != nil {
+		println(err.Error(), ": error creating token transaction")
+		return
+	}
+	// Sign the transaction with the operator key
+	nftSignTransaction := nftCreateTransaction.Sign(operatorKey)
+	// Submit the transaction to the Hedera network
+	nftCreateSubmit, err := nftSignTransaction.Execute(client)
+	if err != nil {
+		println(err.Error(), ": error submitting transaction")
+		return
+	}
+	// Get transaction receipt information
+	nftCreateReceipt, err := nftCreateSubmit.GetReceipt(client)
+	if err != nil {
+		println(err.Error(), ": error receiving receipt")
+		return
+	}
+	// Get token id from the transaction
+	nftTokenID := *nftCreateReceipt.TokenID
+	fmt.Println("Created NFT with token id: ", nftTokenID)
+
+	/**
+	 * Step 2
+	 *
+	 * Mint the NFT
+	 */
+
+	nftCollection := []hedera.TransactionReceipt{}
+
+	for i, s := range cid {
+		mintTransaction, err := hedera.NewTokenMintTransaction().SetTokenID(nftTokenID).SetMetadata([]byte(s)).FreezeWith(client)
+		if err != nil {
+			println(err.Error(), ": error creating mint transaction")
+			return
+		}
+		mintTransactionSubmit, err := mintTransaction.Sign(supplyKey).Execute(client)
+		if err != nil {
+			println(err.Error(), ": error submitting transaction")
+			return
+		}
+		receipt, err := mintTransactionSubmit.GetReceipt(client)
+		if err != nil {
+			println(err.Error(), ": error receiving receipt")
+			return
+		}
+		nftCollection = append(nftCollection, receipt)
+		fmt.Println("Created NFT ", nftTokenID.String(), " with serial: ", nftCollection[i].SerialNumbers[0])
+	}
+	exampleNftId := nftTokenID.Nft(nftCollection[0].SerialNumbers[0])
+	/**
+	 * Step 3
+	 *
+	 * Create an ECDSA public key alias
+	 */
+
+	fmt.Println("Creating new account...")
+	privateKey, err := hedera.PrivateKeyGenerateEcdsa()
+	if err != nil {
+		println(err.Error(), ": error generating private key")
+		return
+	}
+	publicKey := privateKey.PublicKey()
+	// Assuming that the target shard and realm are known.
+	// For now they are virtually always 0 and 0.
+	aliasAccountId := publicKey.ToAccountID(0, 0)
+	fmt.Println("New account ID: ", aliasAccountId)
+	fmt.Println("Just the aliasKey: ", aliasAccountId.AliasKey)
+
+	/**
+	 * Step 4
+	 *
+	 * Tranfer the NFT to the public key alias using the transfer transaction
+	 */
+	nftTransferTransaction, err := hedera.NewTransferTransaction().AddNftTransfer(exampleNftId, operatorId, *aliasAccountId).FreezeWith(client)
+	if err != nil {
+		println(err.Error(), ": error creating transaction")
+		return
+	}
+	// Sign the transaction with the operator key
+	nftTransferTransactionSign := nftTransferTransaction.Sign(operatorKey)
+	// Submit the transaction to the Hedera network
+	nftTransferTransactionSubmit, err := nftTransferTransactionSign.Execute(client)
+	if err != nil {
+		println(err.Error(), ": error submitting transaction")
+		return
+	}
+	// Get transaction receipt information here
+	fmt.Println(nftTransferTransactionSubmit.GetReceipt(client))
+
+	/**
+	 * Step 5
+	 *
+	 * Return the new account ID in the child record
+	 */
+
+	//Returns the info for the specified NFT id
+	nftInfo, err := hedera.NewTokenNftInfoQuery().SetNftID(exampleNftId).Execute(client)
+	if err != nil {
+		println(err.Error(), ": error info query transaction")
+		return
+	}
+	nftOwnerAccountId := nftInfo[0].AccountID
+	fmt.Println("Current owner account id: ", nftOwnerAccountId)
+
+	/**
+	 * Step 6
+	 *
+	 * Show the new account ID owns the NFT
+	 */
+	accountInfo, err := hedera.NewAccountInfoQuery().SetAccountID(*aliasAccountId).Execute(client)
+	if err != nil {
+		println(err.Error(), ": error account info query")
+		return
+	}
+	fmt.Println("The normal account ID of the given alias ", accountInfo.AccountID)
+
+	if nftOwnerAccountId == accountInfo.AccountID {
+		fmt.Println("The NFT owner accountId matches the accountId created with the HTS")
+	} else {
+		fmt.Println("The two account IDs does not match")
+	}
+
+	/**
+	 *     Example 2
+	 *
+	 * Step 1
+	 *
+	 * Create a fungible HTS token using the Hedera Token Service
+	 */
+	fmt.Println("Example 2")
+
+	tokenCreateTransaction, err := hedera.NewTokenCreateTransaction().SetTokenName("HIP-542 Token").
+		SetTokenSymbol("H542").SetTokenType(hedera.TokenTypeFungibleCommon).SetTreasuryAccountID(operatorId).
+		SetInitialSupply(10000).SetDecimals(2).SetAutoRenewAccount(operatorId).FreezeWith(client)
+	if err != nil {
+		println(err.Error(), ": error creating transaction")
+		return
+	}
+	// Sign the transaction with the operator key
+	tokenCreateTransactionSign := tokenCreateTransaction.Sign(operatorKey)
+	// Submit the transaction to the Hedera network
+	tokenCreateTransactionSubmit, err := tokenCreateTransactionSign.Execute(client)
+	if err != nil {
+		println(err.Error(), ": error submitting transaction")
+		return
+	}
+
+	// Get transaction receipt information
+	tokenCreateTransactionReceipt, err := tokenCreateTransactionSubmit.GetReceipt(client)
+	if err != nil {
+		println(err.Error(), ": error retrieving receipt")
+		return
+	}
+	tokenId := *tokenCreateTransactionReceipt.TokenID
+	fmt.Println("Created token with token id: ", tokenId)
+
+	/**
+	 * Step 2
+	 *
+	 * Create an ECDSA public key alias
+	 */
+	privateKey2, err := hedera.PrivateKeyGenerateEcdsa()
+	if err != nil {
+		println(err.Error(), ": error generating private key")
+		return
+	}
+	publicKey2 := privateKey2.PublicKey()
+	// Assuming that the target shard and realm are known.
+	// For now they are virtually always 0 and 0.
+	aliasAccountId2 := *publicKey2.ToAccountID(0, 0)
+	fmt.Println("New account ID: ", aliasAccountId2)
+	fmt.Println("Just the aliasKey: ", aliasAccountId2.AliasKey)
+
+	/**
+	 * Step 3
+	 *
+	 * Transfer the fungible token to the public key alias
+	 */
+
+	tokenTransferTransaction, err := hedera.NewTransferTransaction().
+		AddTokenTransfer(tokenId, operatorId, -10).AddTokenTransfer(tokenId, aliasAccountId2, 10).
+		FreezeWith(client)
+	if err != nil {
+		println(err.Error(), ": error creating transaction")
+		return
+	}
+	// Sign the transaction with the operator key
+	tokenTransferTransactionSign := tokenTransferTransaction.Sign(operatorKey)
+	// Submit the transaction to the Hedera network
+	tokenTransferTransactionSubmit, err := tokenTransferTransactionSign.Execute(client)
+	if err != nil {
+		println(err.Error(), ": error submitting transaction")
+		return
+	}
+	// Get transaction receipt information
+	fmt.Println(tokenTransferTransactionSubmit.GetReceipt(client))
+
+	/**
+	 * Step 4
+	 *
+	 * Return the new account ID in the child record
+	 */
+
+	accountId2Info, err := hedera.NewAccountInfoQuery().SetAccountID(aliasAccountId2).Execute(client)
+	if err != nil {
+		println(err.Error(), ": error executing acount info query")
+		return
+	}
+	accountId2 := accountId2Info.AccountID
+	fmt.Println("The normal account ID of the given alias: ", accountId2)
+
+	/**
+	 * Step 5
+	 *
+	 * Show the new account ID owns the fungible token
+	 */
+
+	accountBalances, err := hedera.NewAccountBalanceQuery().SetAccountID(aliasAccountId2).Execute(client)
+	if err != nil {
+		println(err.Error(), ": error receiving account balance")
+		return
+	}
+
+	tokenBalanceAccountId2 := accountBalances.Tokens.Get(tokenId)
+	if tokenBalanceAccountId2 == 10 {
+		fmt.Println(`Account is created succesfully using HTS "TransferTransaction"`)
+	} else {
+		fmt.Println("Creating account with HTS using public key alias failed")
+	}
+
+}
