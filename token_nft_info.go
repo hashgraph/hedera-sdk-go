e6a6df494b3e5c50881b7275b5c5c3273dbb8dea
Emanuel Pargov <bamzedev@gmail.com>
2022-11-24 11:38:26 +0200

    Change AllowanceSpenderAccountID to SpenderID (#634)
    Signed-off-by: Emanuel Pargov <bamzedev@gmail.com>
    
    Signed-off-by: Emanuel Pargov <bamzedev@gmail.com>

--- token_nft_info.go
+++ token_nft_info.go
@@ -33,9 +33,7 @@ type TokenNftInfo struct {
 	CreationTime time.Time
 	Metadata     []byte
 	LedgerID     LedgerID
-	// Deprecated
-	SpenderID                 AccountID
-	AllowanceSpenderAccountID AccountID
+	SpenderID    AccountID
 }
 
 func _TokenNftInfoFromProtobuf(pb *services.TokenNftInfo) TokenNftInfo {
@@ -54,12 +52,12 @@ func _TokenNftInfoFromProtobuf(pb *services.TokenNftInfo) TokenNftInfo {
 	}
 
 	return TokenNftInfo{
-		NftID:                     _NftIDFromProtobuf(pb.NftID),
-		AccountID:                 accountID,
-		CreationTime:              _TimeFromProtobuf(pb.CreationTime),
-		Metadata:                  pb.Metadata,
-		LedgerID:                  LedgerID{pb.LedgerId},
-		AllowanceSpenderAccountID: spenderID,
+		NftID:        _NftIDFromProtobuf(pb.NftID),
+		AccountID:    accountID,
+		CreationTime: _TimeFromProtobuf(pb.CreationTime),
+		Metadata:     pb.Metadata,
+		LedgerID:     LedgerID{pb.LedgerId},
+		SpenderID:    spenderID,
 	}
 }
 
@@ -70,7 +68,7 @@ func (tokenNftInfo *TokenNftInfo) _ToProtobuf() *services.TokenNftInfo {
 		CreationTime: _TimeToProtobuf(tokenNftInfo.CreationTime),
 		Metadata:     tokenNftInfo.Metadata,
 		LedgerId:     tokenNftInfo.LedgerID.ToBytes(),
-		SpenderId:    tokenNftInfo.AllowanceSpenderAccountID._ToProtobuf(),
+		SpenderId:    tokenNftInfo.SpenderID._ToProtobuf(),
 	}
 }
 
