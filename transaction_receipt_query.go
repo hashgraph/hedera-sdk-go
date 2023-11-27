3a9e2894a9b8affec9fb884bb354d0151393e3ed
NikolaMirchev <nikola.mirchev@limechain.tech>
2023-11-27 17:37:06 +0200

    Refactored transaction_receipt_query.go to match the new design style
    Signed-off-by: NikolaMirchev <nikola.mirchev@limechain.tech>

--- transaction_receipt_query.go
+++ transaction_receipt_query.go
@@ -49,29 +49,31 @@ type TransactionReceiptQuery struct {
 // State proof is available for this response
 func NewTransactionReceiptQuery() *TransactionReceiptQuery {
 	header := services.QueryHeader{}
-	return &TransactionReceiptQuery{
+	result := TransactionReceiptQuery{
 		query: _NewQuery(false, &header),
 	}
+	result.e = &result
+	return &result
 }
 
 // When execution is attempted, a single attempt will timeout when this deadline is reached. (The SDK may subsequently retry the execution.)
-func (query *TransactionReceiptQuery) SetGrpcDeadline(deadline *time.Duration) *TransactionReceiptQuery {
-	query.query.SetGrpcDeadline(deadline)
-	return query
+func (this *TransactionReceiptQuery) SetGrpcDeadline(deadline *time.Duration) *TransactionReceiptQuery {
+	this.query.SetGrpcDeadline(deadline)
+	return this
 }
 
 // SetIncludeChildren Sets whether the response should include the receipts of any child transactions spawned by the
 // top-level transaction with the given transactionID.
-func (query *TransactionReceiptQuery) SetIncludeChildren(includeChildReceipts bool) *TransactionReceiptQuery {
-	query.childReceipts = &includeChildReceipts
-	return query
+func (this *TransactionReceiptQuery) SetIncludeChildren(includeChildReceipts bool) *TransactionReceiptQuery {
+	this.childReceipts = &includeChildReceipts
+	return this
 }
 
 // GetIncludeChildren returns whether the response should include the receipts of any child transactions spawned by the
 // top-level transaction with the given transactionID.
-func (query *TransactionReceiptQuery) GetIncludeChildren() bool {
-	if query.childReceipts != nil {
-		return *query.childReceipts
+func (this *TransactionReceiptQuery) GetIncludeChildren() bool {
+	if this.childReceipts != nil {
+		return *this.childReceipts
 	}
 
 	return false
@@ -82,100 +84,56 @@ func (query *TransactionReceiptQuery) GetIncludeChildren() bool {
 // neither INVALID_NODE_ACCOUNT nor INVALID_PAYER_SIGNATURE; or, if no
 // such receipt exists, the receipt of processing the first transaction to reach consensus with
 // the given transaction id.
-func (query *TransactionReceiptQuery) SetIncludeDuplicates(includeDuplicates bool) *TransactionReceiptQuery {
-	query.duplicates = &includeDuplicates
-	return query
+func (this *TransactionReceiptQuery) SetIncludeDuplicates(includeDuplicates bool) *TransactionReceiptQuery {
+	this.duplicates = &includeDuplicates
+	return this
 }
 
 // GetIncludeDuplicates returns whether receipts of processing duplicate transactions should be returned along with the
 // receipt of processing the first consensus transaction with the given id
-func (query *TransactionReceiptQuery) GetIncludeDuplicates() bool {
-	if query.duplicates != nil {
-		return *query.duplicates
+func (this *TransactionReceiptQuery) GetIncludeDuplicates() bool {
+	if this.duplicates != nil {
+		return *this.duplicates
 	}
 
 	return false
 }
-
-func (query *TransactionReceiptQuery) _ValidateNetworkOnIDs(client *Client) error {
-	if client == nil || !client.autoValidateChecksums {
-		return nil
-	}
-
-	if err := query.transactionID.AccountID.ValidateChecksum(client); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (query *TransactionReceiptQuery) _Build() *services.Query_TransactionGetReceipt {
-	body := &services.TransactionGetReceiptQuery{
-		Header: &services.QueryHeader{},
-	}
-
-	if query.transactionID.AccountID != nil {
-		body.TransactionID = query.transactionID._ToProtobuf()
-	}
-
-	if query.duplicates != nil {
-		body.IncludeDuplicates = *query.duplicates
-	}
-
-	if query.childReceipts != nil {
-		body.IncludeChildReceipts = query.GetIncludeChildren()
-	}
-
-	return &services.Query_TransactionGetReceipt{
-		TransactionGetReceipt: body,
-	}
-}
-
 // GetCost returns the fee that would be charged to get the requested information (if a cost was requested).
-func (query *TransactionReceiptQuery) GetCost(client *Client) (Hbar, error) {
+func (this *TransactionReceiptQuery) GetCost(client *Client) (Hbar, error) {
 	if client == nil {
 		return Hbar{}, errNoClientProvided
 	}
 
 	var err error
 
-	err = query._ValidateNetworkOnIDs(client)
+	err = this.validateNetworkOnIDs(client)
 	if err != nil {
 		return Hbar{}, err
 	}
 
-	query.timestamp = time.Now()
+	this.timestamp = time.Now()
 
-	for range query.nodeAccountIDs.slice {
+	for range this.nodeAccountIDs.slice {
 		paymentTransaction, err := _QueryMakePaymentTransaction(TransactionID{}, AccountID{}, client.operator, Hbar{})
 		if err != nil {
 			return Hbar{}, err
 		}
-		query.paymentTransactions = append(query.paymentTransactions, paymentTransaction)
+		this.paymentTransactions = append(this.paymentTransactions, paymentTransaction)
 	}
 
-	pb := query._Build()
-	pb.TransactionGetReceipt.Header = query.pbHeader
+	pb := this.build()
+	pb.TransactionGetReceipt.Header = this.pbHeader
 
-	query.pb = &services.Query{
+	this.pb = &services.Query{
 		Query: pb,
 	}
 
+	this.pbHeader.ResponseType = services.ResponseType_COST_ANSWER
+	this.paymentTransactionIDs._Advance()
+
 	resp, err := _Execute(
 		client,
-		&query.query,
-		_TransactionReceiptQueryShouldRetry,
-		_CostQueryMakeRequest,
-		_CostQueryAdvanceRequest,
-		getNodeAccountID,
-		_TransactionReceiptQueryGetMethod,
-		_TransactionReceiptQueryMapStatusError,
-		mapResponse,
-		query._GetLogID(),
-		query.grpcDeadline,
-		query.maxBackoff,
-		query.minBackoff,
-		query.maxRetry,
+		this.e,
 	)
 
 	if err != nil {
@@ -186,8 +144,195 @@ func (query *TransactionReceiptQuery) GetCost(client *Client) (Hbar, error) {
 	return HbarFromTinybar(cost), nil
 }
 
-func _TransactionReceiptQueryShouldRetry(request interface{}, response interface{}) _ExecutionState {
-	status := Status(response.(*services.Response).GetTransactionGetReceipt().GetHeader().GetNodeTransactionPrecheckCode())
+// Execute executes the Query with the provided client
+func (this *TransactionReceiptQuery) Execute(client *Client) (TransactionReceipt, error) {
+	if client == nil {
+		return TransactionReceipt{}, errNoClientProvided
+	}
+
+	var err error
+
+	err = this.validateNetworkOnIDs(client)
+	if err != nil {
+		return TransactionReceipt{}, err
+	}
+
+	this.timestamp = time.Now()
+
+	this.paymentTransactions = make([]*services.Transaction, 0)
+
+	pb := this.build()
+	pb.TransactionGetReceipt.Header = this.pbHeader
+	this.pb = &services.Query{
+		Query: pb,
+	}
+
+	if this.isPaymentRequired && len(this.paymentTransactions) > 0 {
+		this.paymentTransactionIDs._Advance()
+	}
+	this.pbHeader.ResponseType = services.ResponseType_ANSWER_ONLY
+
+	resp, err := _Execute(
+		client,
+		this.e,
+	)
+
+	if err, ok := err.(ErrHederaPreCheckStatus); ok {
+		if resp.(*services.Response).GetTransactionGetReceipt() != nil {
+			return _TransactionReceiptFromProtobuf(resp.(*services.Response).GetTransactionGetReceipt(), this.transactionID), err
+		}
+		// Manually add the receipt status, because an empty receipt has no status and no status defaults to 0, which means success
+		return TransactionReceipt{Status: err.Status}, err
+	}
+
+	return _TransactionReceiptFromProtobuf(resp.(*services.Response).GetTransactionGetReceipt(), this.transactionID), nil
+}
+
+
+// SetTransactionID sets the TransactionID for which the receipt is being requested.
+func (this *TransactionReceiptQuery) SetTransactionID(transactionID TransactionID) *TransactionReceiptQuery {
+	this.transactionID = &transactionID
+	return this
+}
+
+// GetTransactionID returns the TransactionID for which the receipt is being requested.
+func (this *TransactionReceiptQuery) GetTransactionID() TransactionID {
+	if this.transactionID == nil {
+		return TransactionID{}
+	}
+
+	return *this.transactionID
+}
+
+// SetNodeAccountIDs sets the _Node AccountID for this TransactionReceiptQuery.
+func (this *TransactionReceiptQuery) SetNodeAccountIDs(accountID []AccountID) *TransactionReceiptQuery {
+	this.query.SetNodeAccountIDs(accountID)
+	return this
+}
+
+// SetQueryPayment sets the Hbar payment to pay the _Node a fee for handling this query
+func (this *TransactionReceiptQuery) SetQueryPayment(queryPayment Hbar) *TransactionReceiptQuery {
+	this.queryPayment = queryPayment
+	return this
+}
+
+// SetMaxQueryPayment sets the maximum payment allowed for this Query.
+func (this *TransactionReceiptQuery) SetMaxQueryPayment(queryMaxPayment Hbar) *TransactionReceiptQuery {
+	this.maxQueryPayment = queryMaxPayment
+	return this
+}
+
+// SetMaxRetry sets the max number of errors before execution will fail.
+func (this *TransactionReceiptQuery) SetMaxRetry(count int) *TransactionReceiptQuery {
+	this.query.SetMaxRetry(count)
+	return this
+}
+
+// SetMaxBackoff The maximum amount of time to wait between retries.
+// Every retry attempt will increase the wait time exponentially until it reaches this time.
+func (this *TransactionReceiptQuery) SetMaxBackoff(max time.Duration) *TransactionReceiptQuery {
+	this.query.SetMaxBackoff(max)
+	return this
+}
+
+// GetMaxBackoff returns the maximum amount of time to wait between retries.
+func (this *TransactionReceiptQuery) GetMaxBackoff() time.Duration {
+	return this.query.GetMaxBackoff()
+}
+
+// SetMinBackoff sets the minimum amount of time to wait between retries.
+func (this *TransactionReceiptQuery) SetMinBackoff(min time.Duration) *TransactionReceiptQuery {
+	this.query.SetMinBackoff(min)
+	return this
+}
+
+// GetMinBackoff returns the minimum amount of time to wait between retries.
+func (this *TransactionReceiptQuery) GetMinBackoff() time.Duration {
+	return this.query.GetMinBackoff()
+}
+
+func (this *TransactionReceiptQuery) _GetLogID() string {
+	timestamp := this.timestamp.UnixNano()
+	return fmt.Sprintf("TransactionReceiptQuery:%d", timestamp)
+}
+
+// SetPaymentTransactionID assigns the payment transaction id.
+func (this *TransactionReceiptQuery) SetPaymentTransactionID(transactionID TransactionID) *TransactionReceiptQuery {
+	this.paymentTransactionIDs._Clear()._Push(transactionID)._SetLocked(true)
+	return this
+}
+
+func (this *TransactionReceiptQuery) SetLogLevel(level LogLevel) *TransactionReceiptQuery {
+	this.query.SetLogLevel(level)
+	return this
+}
+// ---------- Parent functions specific implementation ----------
+
+func (this *TransactionReceiptQuery) getMethod(channel *_Channel) _Method {
+	return _Method{
+		query: channel._GetCrypto().GetTransactionReceipts,
+	}
+}
+
+func (this *TransactionReceiptQuery) mapStatusError(_ interface{}, response interface{}) error {
+	switch Status(response.(*services.Response).GetTransactionGetReceipt().GetHeader().GetNodeTransactionPrecheckCode()) {
+	case StatusPlatformTransactionNotCreated, StatusBusy, StatusUnknown, StatusOk:
+		break
+	default:
+		return ErrHederaPreCheckStatus{
+			Status: Status(response.(*services.Response).GetTransactionGetReceipt().GetHeader().GetNodeTransactionPrecheckCode()),
+		}
+	}
+
+	return ErrHederaPreCheckStatus{
+		Status: Status(response.(*services.Response).GetTransactionGetReceipt().GetReceipt().GetStatus()),
+	}
+}
+
+func (this *TransactionReceiptQuery) getName() string {
+	return "TransactionReceiptQuery"
+}
+
+func (this *TransactionReceiptQuery) build()  *services.Query_TransactionGetReceipt {
+	body := &services.TransactionGetReceiptQuery{
+		Header: &services.QueryHeader{},
+	}
+
+	if this.transactionID.AccountID != nil {
+		body.TransactionID = this.transactionID._ToProtobuf()
+	}
+
+	if this.duplicates != nil {
+		body.IncludeDuplicates = *this.duplicates
+	}
+
+	if this.childReceipts != nil {
+		body.IncludeChildReceipts = this.GetIncludeChildren()
+	}
+
+	return &services.Query_TransactionGetReceipt{
+		TransactionGetReceipt: body,
+	}
+}
+
+func (this *TransactionReceiptQuery) validateNetworkOnIDs(client *Client) error {
+	if client == nil || !client.autoValidateChecksums {
+		return nil
+	}
+
+	if err := this.transactionID.AccountID.ValidateChecksum(client); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (this *TransactionReceiptQuery) getQueryStatus(response interface{}) Status {
+	return  Status(response.(*services.Response).GetTransactionGetReceipt().GetHeader().GetNodeTransactionPrecheckCode())
+}
+
+func (this *TransactionReceiptQuery) shouldRetry(_ interface{}, response interface{}) _ExecutionState {
+	status := this.getQueryStatus(response)
 
 	switch status {
 	case StatusPlatformTransactionNotCreated, StatusBusy, StatusUnknown, StatusReceiptNotFound, StatusRecordNotFound:
@@ -206,172 +351,4 @@ func _TransactionReceiptQueryShouldRetry(request interface{}, response interface
 	default:
 		return executionStateFinished
 	}
-}
-
-func _TransactionReceiptQueryMapStatusError(request interface{}, response interface{}) error {
-	switch Status(response.(*services.Response).GetTransactionGetReceipt().GetHeader().GetNodeTransactionPrecheckCode()) {
-	case StatusPlatformTransactionNotCreated, StatusBusy, StatusUnknown, StatusOk:
-		break
-	default:
-		return ErrHederaPreCheckStatus{
-			Status: Status(response.(*services.Response).GetTransactionGetReceipt().GetHeader().GetNodeTransactionPrecheckCode()),
-		}
-	}
-
-	return ErrHederaPreCheckStatus{
-		Status: Status(response.(*services.Response).GetTransactionGetReceipt().GetReceipt().GetStatus()),
-	}
-}
-
-func _TransactionReceiptQueryGetMethod(_ interface{}, channel *_Channel) _Method {
-	return _Method{
-		query: channel._GetCrypto().GetTransactionReceipts,
-	}
-}
-
-// SetTransactionID sets the TransactionID for which the receipt is being requested.
-func (query *TransactionReceiptQuery) SetTransactionID(transactionID TransactionID) *TransactionReceiptQuery {
-	query.transactionID = &transactionID
-	return query
-}
-
-// GetTransactionID returns the TransactionID for which the receipt is being requested.
-func (query *TransactionReceiptQuery) GetTransactionID() TransactionID {
-	if query.transactionID == nil {
-		return TransactionID{}
-	}
-
-	return *query.transactionID
-}
-
-// SetNodeAccountIDs sets the _Node AccountID for this TransactionReceiptQuery.
-func (query *TransactionReceiptQuery) SetNodeAccountIDs(accountID []AccountID) *TransactionReceiptQuery {
-	query.query.SetNodeAccountIDs(accountID)
-	return query
-}
-
-// SetQueryPayment sets the Hbar payment to pay the _Node a fee for handling this query
-func (query *TransactionReceiptQuery) SetQueryPayment(queryPayment Hbar) *TransactionReceiptQuery {
-	query.queryPayment = queryPayment
-	return query
-}
-
-// SetMaxQueryPayment sets the maximum payment allowed for this Query.
-func (query *TransactionReceiptQuery) SetMaxQueryPayment(queryMaxPayment Hbar) *TransactionReceiptQuery {
-	query.maxQueryPayment = queryMaxPayment
-	return query
-}
-
-// SetMaxRetry sets the max number of errors before execution will fail.
-func (query *TransactionReceiptQuery) SetMaxRetry(count int) *TransactionReceiptQuery {
-	query.query.SetMaxRetry(count)
-	return query
-}
-
-// SetMaxBackoff The maximum amount of time to wait between retries.
-// Every retry attempt will increase the wait time exponentially until it reaches this time.
-func (query *TransactionReceiptQuery) SetMaxBackoff(max time.Duration) *TransactionReceiptQuery {
-	if max.Nanoseconds() < 0 {
-		panic("maxBackoff must be a positive duration")
-	} else if max.Nanoseconds() < query.minBackoff.Nanoseconds() {
-		panic("maxBackoff must be greater than or equal to minBackoff")
-	}
-	query.maxBackoff = &max
-	return query
-}
-
-// GetMaxBackoff returns the maximum amount of time to wait between retries.
-func (query *TransactionReceiptQuery) GetMaxBackoff() time.Duration {
-	if query.maxBackoff != nil {
-		return *query.maxBackoff
-	}
-
-	return 8 * time.Second
-}
-
-// SetMinBackoff sets the minimum amount of time to wait between retries.
-func (query *TransactionReceiptQuery) SetMinBackoff(min time.Duration) *TransactionReceiptQuery {
-	if min.Nanoseconds() < 0 {
-		panic("minBackoff must be a positive duration")
-	} else if query.maxBackoff.Nanoseconds() < min.Nanoseconds() {
-		panic("minBackoff must be less than or equal to maxBackoff")
-	}
-	query.minBackoff = &min
-	return query
-}
-
-// GetMinBackoff returns the minimum amount of time to wait between retries.
-func (query *TransactionReceiptQuery) GetMinBackoff() time.Duration {
-	if query.minBackoff != nil {
-		return *query.minBackoff
-	}
-
-	return 250 * time.Millisecond
-}
-
-// Execute executes the Query with the provided client
-func (query *TransactionReceiptQuery) Execute(client *Client) (TransactionReceipt, error) {
-	if client == nil {
-		return TransactionReceipt{}, errNoClientProvided
-	}
-
-	var err error
-
-	err = query._ValidateNetworkOnIDs(client)
-	if err != nil {
-		return TransactionReceipt{}, err
-	}
-
-	query.timestamp = time.Now()
-
-	query.paymentTransactions = make([]*services.Transaction, 0)
-
-	pb := query._Build()
-	pb.TransactionGetReceipt.Header = query.pbHeader
-	query.pb = &services.Query{
-		Query: pb,
-	}
-
-	resp, err := _Execute(
-		client,
-		&query.query,
-		_TransactionReceiptQueryShouldRetry,
-		makeRequest,
-		advanceRequest,
-		getNodeAccountID,
-		_TransactionReceiptQueryGetMethod,
-		_TransactionReceiptQueryMapStatusError,
-		mapResponse,
-		query._GetLogID(),
-		query.grpcDeadline,
-		query.maxBackoff,
-		query.minBackoff,
-		query.maxRetry,
-	)
-
-	if err, ok := err.(ErrHederaPreCheckStatus); ok {
-		if resp.(*services.Response).GetTransactionGetReceipt() != nil {
-			return _TransactionReceiptFromProtobuf(resp.(*services.Response).GetTransactionGetReceipt(), query.transactionID), err
-		}
-		// Manually add the receipt status, because an empty receipt has no status and no status defaults to 0, which means success
-		return TransactionReceipt{Status: err.Status}, err
-	}
-
-	return _TransactionReceiptFromProtobuf(resp.(*services.Response).GetTransactionGetReceipt(), query.transactionID), nil
-}
-
-func (query *TransactionReceiptQuery) _GetLogID() string {
-	timestamp := query.timestamp.UnixNano()
-	return fmt.Sprintf("TransactionReceiptQuery:%d", timestamp)
-}
-
-// SetPaymentTransactionID assigns the payment transaction id.
-func (query *TransactionReceiptQuery) SetPaymentTransactionID(transactionID TransactionID) *TransactionReceiptQuery {
-	query.paymentTransactionIDs._Clear()._Push(transactionID)._SetLocked(true)
-	return query
-}
-
-func (query *TransactionReceiptQuery) SetLogLevel(level LogLevel) *TransactionReceiptQuery {
-	query.query.SetLogLevel(level)
-	return query
 }
