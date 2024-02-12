da506d2e74eaa47af3ffde2cb785c03b1f0bbcea
NikolaMirchev <nikola.mirchev@limechain.tech>
2024-02-12 15:48:42 +0200

    Added new methods for accountCreate/Update/Delete inside tck server
    Signed-off-by: NikolaMirchev <nikola.mirchev@limechain.tech>

--- tck/cmd/server.go
+++ tck/cmd/server.go
@@ -8,6 +8,8 @@ import (
 	"github.com/joho/godotenv"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -22,12 +24,18 @@ func main() {
 
 	// Create a new RPC server
 	assigner := handler.Map{
-		"setup":              handler.New(sdkService.Setup),
-		"reset":              handler.New(sdkService.Reset),
-		"createAccount":      handler.New(accountService.CreateAccount),
-		"generatePublicKey":  handler.New(keyService.GeneratePublicKey),
-		"generatePrivateKey": handler.New(keyService.GeneratePrivateKey),
+		"setup":                  handler.New(sdkService.Setup),
+		"reset":                  handler.New(sdkService.Reset),
+		"createAccount":          handler.New(accountService.CreateAccount),
+		"createAccountFromAlias": handler.New(accountService.CreateAccountFromAlias),
+		"getAccountInfo":         handler.New(accountService.GetAccountInfo),
+		"updateAccountKey":       handler.New(accountService.UpdateAccountKey),
+		"updateAccountMemo":      handler.New(accountService.UpdateAccountMemo),
+		"deleteAccount":          handler.New(accountService.DeleteAccount),
+		"generatePublicKey":      handler.New(keyService.GeneratePublicKey),
+		"generatePrivateKey":     handler.New(keyService.GeneratePrivateKey),
 	}
+
 	bridge := jhttp.NewBridge(assigner, nil)
 
 	// Listen and redirect to bridge
@@ -37,8 +45,15 @@ func main() {
 		port = "80"
 	}
 	fmt.Println("Server is listening on port: " + port)
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		panic(err)
-	}
+	go func() {
+		http.ListenAndServe(":"+port, nil)
+	}()
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+	sig := <-signalCh
+	fmt.Printf("Received signal: %v\n", sig)
+
+	// Exit the application
+	os.Exit(0)
 }
