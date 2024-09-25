2e105bde31f2e4812666a1dfc6fa9aa3419050fd
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2024-09-25 11:35:02 +0300

    chore: set error level for e2e test logger
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- utilities_for_test.go
+++ utilities_for_test.go
@@ -118,7 +118,7 @@ func NewIntegrationTestEnv(t *testing.T) IntegrationTestEnv {
 	env.Client.SetMaxAttempts(11)
 	env.Client.SetDefaultMaxTransactionFee(NewHbar(50))
 	env.Client.SetDefaultMaxQueryPayment(NewHbar(50))
-	logger := NewLogger("Hedera sdk", LoggerLevelDebug)
+	logger := NewLogger("Hedera sdk", LoggerLevelError)
 	env.Client.SetLogger(logger)
 
 	env.OriginalOperatorID = env.Client.GetOperatorAccountID()
