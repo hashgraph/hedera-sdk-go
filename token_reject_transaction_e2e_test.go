a24c052c622f7cbe831c6553a50ccf66e1f76245
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2024-10-09 16:26:14 +0300

    fix build
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>

--- token_reject_transaction_e2e_test.go
+++ token_reject_transaction_e2e_test.go
@@ -643,8 +643,6 @@ func TestIntegrationTokenRejectTransactionDoesNotRemoveAllowanceNFT(t *testing.T
 	require.NoError(t, err)
 	env.Client.SetOperator(env.OperatorID, env.OperatorKey)
 
-	tokenInfo, _ := NewTokenNftInfoQuery().SetNftID(nftID.Nft(serials[1])).Execute(env.Client)
-
 	// reject the token
 	frozenTxn, err := NewTokenRejectTransaction().
 		SetOwnerID(receiver).
@@ -655,8 +653,6 @@ func TestIntegrationTokenRejectTransactionDoesNotRemoveAllowanceNFT(t *testing.T
 	require.NoError(t, err)
 	_, err = resp.SetValidateStatus(true).GetReceipt(env.Client)
 	require.NoError(t, err)
-
-	tokenInfo, _ = NewTokenNftInfoQuery().SetNftID(nftID.Nft(serials[1])).Execute(env.Client)
 
 	// verify the allowance - should be 0 , because the receiver is no longer the owner
 	env.Client.SetOperator(spender, spenderKey)
